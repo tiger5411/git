abeb7f01859644ee1372a5d413f90e5b030636c7
Jonathan Nieder <jrnieder@gmail.com>
2010-07-24 20:01:16 -0500

    commit --dry-run: give advice on empty amend
    Share code from the non-dry-run case to ensure the output from
    "commit --amend --dry-run" matches that from "commit --amend"
    on attempts to amend away a commit.  The output had fallen
    out of synch in v1.7.1.1~16^2 (2010-06-06).
    
    The only change in output is some extra text to stderr.
    
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/commit.c
+++ builtin/commit.c
@@ -1016,7 +1016,14 @@ static int dry_run_commit(int argc, const char **argv, const char *prefix,
 	const char *index_file;
 
 	index_file = prepare_index(argc, argv, prefix, 1);
-	commitable = run_status(stdout, index_file, prefix, 0, s);
+
+	/*
+	 * Give extra advice when faced with attempts to amend away a commit.
+	 */
+	if (!something_is_staged() && !empty_commit_ok(index_file, prefix, s))
+		commitable = 0;
+	else
+		commitable = run_status(stdout, index_file, prefix, 0, s);
 	rollback_index_files();
 
 	return commitable ? 0 : 1;
