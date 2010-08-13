47e14f9af02ff9afa55f9bf962b902a35b6566c7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-08-13 17:55:11 +0000

    blah blah use perl path for annotate
    perl added in:
    
    commit 92a903acfd0904e6dd6d18112428429938783d19
    Author: Junio C Hamano <junkio@cox.net>
    Date:   Sun Mar 5 22:07:37 2006 -0800
    
        annotate/blame tests updates.

--- t/annotate-tests.sh
+++ t/annotate-tests.sh
@@ -6,7 +6,7 @@ check_count () {
 	case "$1" in -h) head="$2"; shift; shift ;; esac
 	echo "$PROG file $head" >&4
 	$PROG file $head >.result || return 1
-	cat .result | perl -e '
+	cat .result | "$PERL_PATH" -e '
 		my %expect = (@ARGV);
 		my %count = ();
 		while (<STDIN>) {
@@ -34,84 +34,84 @@ check_count () {
 	' "$@"
 }
 
-test_expect_success \
+test_expect_success PERL\
     'prepare reference tree' \
     'echo "1A quick brown fox jumps over the" >file &&
      echo "lazy dog" >>file &&
      git add file
      GIT_AUTHOR_NAME="A" git commit -a -m "Initial."'
 
-test_expect_success \
+test_expect_success PERL\
     'check all lines blamed on A' \
     'check_count A 2'
 
-test_expect_success \
+test_expect_success PERL\
     'Setup new lines blamed on B' \
     'echo "2A quick brown fox jumps over the" >>file &&
      echo "lazy dog" >> file &&
      GIT_AUTHOR_NAME="B" git commit -a -m "Second."'
 
-test_expect_success \
+test_expect_success PERL\
     'Two lines blamed on A, two on B' \
     'check_count A 2 B 2'
 
-test_expect_success \
+test_expect_success PERL\
     'merge-setup part 1' \
     'git checkout -b branch1 master &&
      echo "3A slow green fox jumps into the" >> file &&
      echo "well." >> file &&
      GIT_AUTHOR_NAME="B1" git commit -a -m "Branch1-1"'
 
-test_expect_success \
+test_expect_success PERL\
     'Two lines blamed on A, two on B, two on B1' \
     'check_count A 2 B 2 B1 2'
 
-test_expect_success \
+test_expect_success PERL\
     'merge-setup part 2' \
     'git checkout -b branch2 master &&
      sed -e "s/2A quick brown/4A quick brown lazy dog/" < file > file.new &&
      mv file.new file &&
      GIT_AUTHOR_NAME="B2" git commit -a -m "Branch2-1"'
 
-test_expect_success \
+test_expect_success PERL\
     'Two lines blamed on A, one on B, one on B2' \
     'check_count A 2 B 1 B2 1'
 
-test_expect_success \
+test_expect_success PERL\
     'merge-setup part 3' \
     'git pull . branch1'
 
-test_expect_success \
+test_expect_success PERL\
     'Two lines blamed on A, one on B, two on B1, one on B2' \
     'check_count A 2 B 1 B1 2 B2 1'
 
-test_expect_success \
+test_expect_success PERL\
     'Annotating an old revision works' \
     'check_count -h master A 2 B 2'
 
-test_expect_success \
+test_expect_success PERL\
     'Annotating an old revision works' \
     'check_count -h master^ A 2'
 
-test_expect_success \
+test_expect_success PERL\
     'merge-setup part 4' \
     'echo "evil merge." >>file &&
      git commit -a --amend'
 
-test_expect_success \
+test_expect_success PERL\
     'Two lines blamed on A, one on B, two on B1, one on B2, one on A U Thor' \
     'check_count A 2 B 1 B1 2 B2 1 "A U Thor" 1'
 
-test_expect_success \
+test_expect_success PERL\
     'an incomplete line added' \
     'echo "incomplete" | tr -d "\\012" >>file &&
     GIT_AUTHOR_NAME="C" git commit -a -m "Incomplete"'
 
-test_expect_success \
+test_expect_success PERL\
     'With incomplete lines.' \
     'check_count A 2 B 1 B1 2 B2 1 "A U Thor" 1 C 1'
 
-test_expect_success \
+test_expect_success PERL\
     'some edit' \
     'mv file file.orig &&
     {
@@ -121,6 +121,6 @@ test_expect_success \
     echo "incomplete" | tr -d "\\012" >>file &&
     GIT_AUTHOR_NAME="D" git commit -a -m "edit"'
 
-test_expect_success \
+test_expect_success PERL\
     'some edit' \
     'check_count A 1 B 1 B1 1 B2 1 "A U Thor" 1 C 1 D 1'
--- t/t8001-annotate.sh
+++ t/t8001-annotate.sh
@@ -6,7 +6,7 @@ test_description='git annotate'
 PROG='git annotate'
 . "$TEST_DIRECTORY"/annotate-tests.sh
 
-test_expect_success \
+test_expect_success PERL \
     'Annotating an old revision works' \
     '[ $(git annotate file master | awk "{print \$3}" | grep -c "^A$") -eq 2 ] && \
      [ $(git annotate file master | awk "{print \$3}" | grep -c "^B$") -eq 2 ]'
