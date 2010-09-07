8ecb4788f256756c27524a0a56ba24ce23244126
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-09-07 11:03:45 +0000

    gettextize: git-clean clean.requireForce braces
    Use braces around a nested if/if/else to appease GCC, which'll whine
    under -Wall and "suggest explicit braces to avoid ambiguous ‘else’".
    
    This code was originally added in "gettextize: git-clean
    clean.requireForce messages", and tested under clang (which doesn't
    whine about perfectly valid uses of C).
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/clean.c
+++ builtin/clean.c
@@ -77,13 +77,14 @@ int cmd_clean(int argc, const char **argv, const char *prefix)
 	if (ignored && ignored_only)
 		die(_("-x and -X cannot be used together"));
 
-	if (!show_only && !force)
+	if (!show_only && !force) {
 		if (config_set)
 			die(_("clean.requireForce set to true and neither -n nor -f given; "
 				  "refusing to clean"));
 		else
 			die(_("clean.requireForce defaults to true and neither -n nor -f given; "
 				  "refusing to clean"));
+	}
 
 	if (force > 1)
 		rm_flags = 0;
