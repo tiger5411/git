e29542d3dbbd2221f2c33b47dfa40fe8cd6fa0d5
Jonathan Nieder <jrnieder@gmail.com>
2010-10-13 04:30:37 -0500

    vcs-svn: read the preimage when applying deltas
    The source view offset heading each svndiff0 window represents a
    number of bytes past the beginning of the preimage.  Together with the
    source view length, it dictates to the delta applier what portion of
    the preimage instructions will refer to.  Read that portion right away
    using the sliding window code.
    
    Maybe some day we will use mmap to read data more lazily.
    
    Subversion's implementation tolerates source view offsets pointing
    past the end of the preimage file but we do not, for simplicity.
    
    This does not teach the delta applier to read instructions or copy
    data from the source view.  Deltas that could produce nonempty output
    will still be rejected.
    
    Improved-by: Ramkumar Ramachandra <artagnon@gmail.com>
    Improved-by: David Barr <david.barr@cordelta.com>
    Signed-off-by: Jonathan Nieder <jrnieder@gmail.com>
    Acked-by: Ramkumar Ramachandra <artagnon@gmail.com>

--- vcs-svn/svndiff.c
+++ vcs-svn/svndiff.c
@@ -4,6 +4,7 @@
  */
 
 #include "git-compat-util.h"
+#include "sliding_window.h"
 #include "line_buffer.h"
 #include "svndiff.h"
 
@@ -122,11 +123,12 @@ int svndiff0_apply(struct line_buffer *delta, off_t delta_len,
 	if (read_magic(delta, &delta_len))
 		return -1;
 	while (delta_len) {	/* For each window: */
-		off_t pre_off;
-		size_t pre_len;
+		off_t pre_off = pre_off;
+		size_t pre_len = pre_len;
 
 		if (read_offset(delta, &pre_off, &delta_len) ||
 		    read_length(delta, &pre_len, &delta_len) ||
+		    move_window(preimage, pre_off, pre_len) ||
 		    apply_one_window(delta, &delta_len))
 			return -1;
 	}
