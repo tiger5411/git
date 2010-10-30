0a0aee4f1c73f4280b28d6224fb2ffd6496f0ac0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-10-30 19:49:08 +0000

    gettextize: git-clone: !fixup "basic messages"
    Gettextize the "--depth is ignored in local clones" message added in
    v1.7.3-rc0~8^2 by Nguyễn Thái Ngọc Duy.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/clone.c
+++ builtin/clone.c
@@ -415,7 +415,7 @@ int cmd_clone(int argc, const char **argv, const char *prefix)
 		repo = repo_name;
 	is_local = path && !is_bundle;
 	if (is_local && option_depth)
-		warning("--depth is ignored in local clones; use file:// instead.");
+		warning(_("--depth is ignored in local clones; use file:// instead."));
 
 	if (argc == 2)
 		dir = xstrdup(argv[1]);
