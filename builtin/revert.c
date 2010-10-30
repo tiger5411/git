e9d74a75cf35a5848df6cdb38bbb980fbf6a16fc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-10-30 19:55:40 +0000

    gettextize: git-revert: !fixup "Your local changes"
    Gettextize two messages that I missed in 'gettextize: git-revert "Your
    local changes" message'.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/revert.c
+++ builtin/revert.c
@@ -294,9 +294,9 @@ static NORETURN void die_dirty_index(const char *me)
 					  "Please, commit your changes or stash them to proceed."));
 		} else {
 			if (action == REVERT)
-				die("Your local changes would be overwritten by revert.\n");
+				die(_("Your local changes would be overwritten by revert.\n"));
 			else
-				die("Your local changes would be overwritten by cherry-pick.\n");
+				die(_("Your local changes would be overwritten by cherry-pick.\n"));
 		}
 	}
 }
