06924435e30f307dadae43dbd48fbc9f5a453215
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-10-30 19:56:48 +0000

    gettextize: git-merge: !fixup "basic messages"
    Gettextize a "%s: negative length %s" error message in builtin/merge.c
    that I missed the first time around.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -511,7 +511,7 @@ static int git_merge_config(const char *k, const char *v, void *cb)
 		int is_bool;
 		shortlog_len = git_config_bool_or_int(k, v, &is_bool);
 		if (!is_bool && shortlog_len < 0)
-			return error("%s: negative length %s", k, v);
+			return error(_("%s: negative length %s"), k, v);
 		if (is_bool && shortlog_len)
 			shortlog_len = DEFAULT_MERGE_LOG_LEN;
 		return 0;
