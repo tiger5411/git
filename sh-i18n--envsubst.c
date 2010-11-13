7b2cfe8d50f0b22410a4d45162bf2bd422424c4b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2010-11-13 20:23:31 +0100

    nuke string_list_init / string_list_append

--- sh-i18n--envsubst.c
+++ sh-i18n--envsubst.c
@@ -185,33 +185,6 @@ struct string_list_ty
   size_t nitems_max;
 };
 
-/* Initialize an empty list of strings.  */
-static inline void
-string_list_init (string_list_ty *slp)
-{
-  slp->item = NULL;
-  slp->nitems = 0;
-  slp->nitems_max = 0;
-}
-
-/* Append a single string to the end of a list of strings.  */
-static inline void
-string_list_append (string_list_ty *slp, const char *s)
-{
-  /* Grow the list.  */
-  if (slp->nitems >= slp->nitems_max)
-    {
-      size_t nbytes;
-
-      slp->nitems_max = slp->nitems_max * 2 + 4;
-      nbytes = slp->nitems_max * sizeof (slp->item[0]);
-      slp->item = (const char **) xrealloc (slp->item, nbytes);
-    }
-
-  /* Add the string to the end of the list.  */
-  slp->item[slp->nitems++] = s;
-}
-
 /* Compare two strings given by reference.  */
 static int
 cmp_string (const void *pstr1, const void *pstr2)
