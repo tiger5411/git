70b39b034e2e1c434ecd445c5181f9ca0b26d15c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-12 23:03:27 +0000

    !fixup: builtin/revert.c new strings since $(git merge-base origin/master ab/i18n-2010-11-16-rebased-on-new-master)

--- builtin/revert.c
+++ builtin/revert.c
@@ -579,12 +579,12 @@ static void read_and_refresh_cache(const char *me)
 	static struct lock_file index_lock;
 	int index_fd = hold_locked_index(&index_lock, 0);
 	if (read_index_preload(&the_index, NULL) < 0)
-		die("git %s: failed to read the index", me);
+		die(_("git %s: failed to read the index"), me);
 	refresh_index(&the_index, REFRESH_QUIET|REFRESH_UNMERGED, NULL, NULL, NULL);
 	if (the_index.cache_changed) {
 		if (write_index(&the_index, index_fd) ||
 		    commit_locked_index(&index_lock))
-			die("git %s: failed to refresh the index", me);
+			die(_("git %s: failed to refresh the index"), me);
 	}
 	rollback_lock_file(&index_lock);
 }
