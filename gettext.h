fe48a81e1620ff162390325452f632ab1bad61cb
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-02-19 18:54:39 +0000

    gettext.h: add no-op _() and N_() wrappers
    Add a new header called gettext.h which is currently a no-op. To split
    up the gettext series I'm first submitting patches to gettextize the
    source tree before I add any of the Makefile and C changes needed to
    actually use them.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

---
+++ gettext.h
@@ -0,0 +1,9 @@
+/*
+ * Copyright (c) 2010 Ævar Arnfjörð Bjarmason
+ *
+ * This is a skeleton no-op implementation of gettext for Git. It'll
+ * be replaced by something that uses libintl.h and wraps gettext() in
+ * a future patch series.
+ */
+#define N_(s) (s)
+#define _(s) (s)
