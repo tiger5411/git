e0e30ebb6c6c3cf26c2bdf11d923fb4a44227dff
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-05-08 10:31:07 +0000

    t/t4151-am-abort.sh: fixup for using the i18ncmp and i18ngrep functions

--- t/t4151-am-abort.sh
+++ t/t4151-am-abort.sh
@@ -43,10 +43,11 @@ do
 		test_cmp expect actual
 	'
 
-	test_expect_success C_LOCALE_OUTPUT "am$with3 --skip continue after failed am$with3" '
+	test_expect_success "am$with3 --skip continue after failed am$with3" '
 		test_must_fail git am$with3 --skip >output &&
-		test "$(grep "^Applying" output)" = "Applying: 6" &&
-		test_cmp file-2-expect file-2 &&
+		test_i18ngrep "^Applying" output >output.applying &&
+		test_i18ngrep "^Applying: 6$" output.applying &&
+		test_i18ncmp file-2-expect file-2 &&
 		test ! -f .git/MERGE_RR
 	'
 
