0503c502b633f25123dfff0c2bbdc5537729da01
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2011-05-08 10:29:06 +0000

    t/t0201-gettext-fallbacks.sh: skip except under i18ncmp

--- t/t0201-gettext-fallbacks.sh
+++ t/t0201-gettext-fallbacks.sh
@@ -11,25 +11,25 @@ test_description='Gettext Shell fallbacks'
 test_expect_success 'gettext: our gettext() fallback has pass-through semantics' '
     printf "test" >expect &&
     gettext "test" >actual &&
-    test_cmp expect actual &&
+    test_i18ncmp expect actual &&
     printf "test more words" >expect &&
     gettext "test more words" >actual &&
-    test_cmp expect actual
+    test_i18ncmp expect actual
 '
 
 test_expect_success 'eval_gettext: our eval_gettext() fallback has pass-through semantics' '
     printf "test" >expect &&
     eval_gettext "test" >actual &&
-    test_cmp expect actual &&
+    test_i18ncmp expect actual &&
     printf "test more words" >expect &&
     eval_gettext "test more words" >actual &&
-    test_cmp expect actual
+    test_i18ncmp expect actual
 '
 
 test_expect_success 'eval_gettext: our eval_gettext() fallback can interpolate variables' '
     printf "test YesPlease" >expect &&
     GIT_INTERNAL_GETTEXT_TEST_FALLBACKS=YesPlease eval_gettext "test \$GIT_INTERNAL_GETTEXT_TEST_FALLBACKS" >actual &&
-    test_cmp expect actual
+    test_i18ncmp expect actual
 '
 
 test_expect_success 'eval_gettext: our eval_gettext() fallback can interpolate variables with spaces' '
@@ -37,7 +37,7 @@ test_expect_success 'eval_gettext: our eval_gettext() fallback can interpolate v
     export cmdline;
     printf "When you have resolved this problem run git am --resolved." >expect &&
     eval_gettext "When you have resolved this problem run \$cmdline --resolved." >actual
-    test_cmp expect actual
+    test_i18ncmp expect actual
 '
 
 test_expect_success 'eval_gettext: our eval_gettext() fallback can interpolate variables with spaces and quotes' '
@@ -45,7 +45,7 @@ test_expect_success 'eval_gettext: our eval_gettext() fallback can interpolate v
     export cmdline;
     printf "When you have resolved this problem run \"git am --resolved\"." >expect &&
     eval_gettext "When you have resolved this problem run \"\$cmdline --resolved\"." >actual
-    test_cmp expect actual
+    test_i18ncmp expect actual
 '
 
 test_done
