ed36655230e1f096055837cec2e2c29b5c03fa14
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2012-01-26 21:01:16 +0000

    ignore more files

--- helper-i18n-subst-candidates.sh
+++ helper-i18n-subst-candidates.sh
@@ -4,4 +4,5 @@ git grep -l '\b(eval_gettext|gettext|gettextln|eval_gettextln)\b' -- *.sh \
 	| grep -v \
 	-e util-remove-i18n.sh \
 	-e git-sh-i18n.sh \
-	-e helper-i18n-subst-candidates.sh
+	-e helper-i18n-subst-candidates.sh \
+	-e util-remove-i18n.sh
