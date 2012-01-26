a8ab692f54c7336481bafdc9e9d92c9990a2f43c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2012-01-26 20:12:42 +0000

    ignore self

--- helper-i18n-subst-candidates.sh
+++ helper-i18n-subst-candidates.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 
-git grep -l '\b(eval_gettext|gettext|gettextln|eval_gettextln)\b' -- *.sh | grep -v -e util-remove-i18n.sh -e git-sh-i18n.sh
+git grep -l '\b(eval_gettext|gettext|gettextln|eval_gettextln)\b' -- *.sh \
+	| grep -v \
+	-e util-remove-i18n.sh \
+	-e git-sh-i18n.sh \
+	-e helper-i18n-subst-candidates.sh
