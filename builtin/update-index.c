471926f13a03eee5d26510260a99214a483afa22
Christian Couder <chriscool@tuxfamily.org>
2015-11-26 17:30:54 +0100

    update-index: add untracked cache notifications
    Attempting to flip the untracked-cache feature on for a random index
    file with
    
        cd /random/unrelated/place
        git --git-dir=/somewhere/else/.git update-index --untracked-cache
    
    would not work as you might expect. Because flipping the feature on
    in the index also records the location of the corresponding working
    tree (/random/unrelated/place in the above example), when the index
    is subsequently used to keep track of files in the working tree in
    /somewhere/else, the feature is disabled.
    
    With this patch "git update-index --untracked-cache" tells the user in
    which directory tests are performed. This makes it easy to spot any
    problem.
    
    Signed-off-by: Christian Couder <chriscool@tuxfamily.org>

--- builtin/update-index.c
+++ builtin/update-index.c
@@ -130,7 +130,7 @@ static int test_if_untracked_cache_is_supported(void)
 	if (!mkdtemp(mtime_dir.buf))
 		die_errno("Could not make temporary directory");
 
-	fprintf(stderr, _("Testing "));
+	fprintf(stderr, _("Testing mtime in '%s' "), xgetcwd());
 	atexit(remove_test_directory);
 	xstat_mtime_dir(&st);
 	fill_stat_data(&base, &st);
@@ -1135,9 +1135,13 @@ int cmd_update_index(int argc, const char **argv, const char *prefix)
 		}
 		add_untracked_ident(the_index.untracked);
 		the_index.cache_changed |= UNTRACKED_CHANGED;
+		if (verbose)
+			printf(_("Untracked cache enabled\n"));
 	} else if (untracked_cache == UC_DISABLE && the_index.untracked) {
 		the_index.untracked = NULL;
 		the_index.cache_changed |= UNTRACKED_CHANGED;
+		if (verbose)
+			printf(_("Untracked cache disabled\n"));
 	}
 
 	if (active_cache_changed) {
