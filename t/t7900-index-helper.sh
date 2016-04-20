szeder/all-git-forks

index-helper: don't run if already running
Signed-off-by: David Turner <dturner@twopensource.com>

t/t7900-index-helper.sh
@@ -38,4 +38,13 @@ test_expect_success 'index-helper creates usable path file and can be killed' '
 	test_path_is_missing .git/index-helper.path
 '
 
+test_expect_success 'index-helper will not start if already running' '
+	test_when_finished "git index-helper --kill" &&
+	git index-helper --detach &&
+	test -L .git/index-helper.path &&
+	test_must_fail git index-helper 2>err &&
+	test -L .git/index-helper.path &&
+	grep "Already running" err
+'
+
 test_done

tiger5411/git

t/t7900-index-helper.sh
@@ -29,4 +29,13 @@ test_expect_success 'index-helper creates usable path file and can be killed' '
 	test_path_is_missing .git/index-helper.sock
 '
 
+test_expect_success 'index-helper will not start if already running' '
+	test_when_finished "git index-helper --kill" &&
+	git index-helper --detach &&
+	test -S .git/index-helper.sock &&
+	test_must_fail git index-helper 2>err &&
+	test -S .git/index-helper.sock &&
+	grep "Already running" err
+'
+
 test_done