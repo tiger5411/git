8c3e386b4f41961249b5c413790f836e3ef40cb6
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-03-27 19:04:23 +0000

    fix this again

--- t/helper/test-mktemp.c
+++ t/helper/test-mktemp.c
@@ -5,7 +5,10 @@
 
 int cmd_main(int argc, const char **argv)
 {
-	fprintf(stderr, "hi there\n");
+	if (argc != 2)
+		usage("Expected 1 parameter defining the temporary file template");
+
+	xmkstemp(xstrdup(argv[1]));
 
 	return 0;
 }
