8b1b2d3a2d46805d4113cceb21747b576f4110fc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-04-21 19:51:11 +0000

    less retarded perf test

--- t/perf/p7821-grep-engines-fixed.sh
+++ t/perf/p7821-grep-engines-fixed.sh
@@ -7,29 +7,20 @@ test_description="Comparison of git-grep's regex engines"
 test_perf_large_repo
 test_checkout_worktree
 
-for engine in fixed basic extended perl
+for args in 'int' '-i int' 'æ' '-i æ'
 do
-	test_perf "$engine grep int"    "
-		git -c grep.patternType=$engine grep int >out.$engine.int || :
-	"
-	test_perf "$engine grep -i int" "
-		git -c grep.patternType=$engine grep -i int >out.$engine.i.int || :
-	"
-	test_perf "$engine grep æ"      "
-		git -c grep.patternType=$engine grep æ >out.$engine.æ || :
-	"
-	test_perf "$engine grep -i æ"      "
-		git -c grep.patternType=$engine grep -i æ >out.$engine.i.æ || :
-	"
-done
+	for engine in fixed basic extended perl
+	do
+		test_perf "$engine grep $args" "
+			git -c grep.patternType=$engine grep $args >'out.$engine.$args' || :
+		"
+	done
 
-for suffix in int i.int æ i.æ
-do  
-	test_expect_success "assert that the $suffix patterns found the same things under all engines" "
-		test_cmp out.fixed.$suffix out.basic.$suffix &&
-		test_cmp out.fixed.$suffix out.extended.$suffix &&
-		test_cmp out.fixed.$suffix out.extended.$suffix &&
-		test_cmp out.fixed.$suffix out.perl.$suffix
+	test_expect_success "assert that all engines found the same for $args" "
+		test_cmp 'out.fixed.$args' 'out.basic.$args' &&
+		test_cmp 'out.fixed.$args' 'out.extended.$args' &&
+		test_cmp 'out.fixed.$args' 'out.extended.$args' &&
+		test_cmp 'out.fixed.$args' 'out.perl.$args'
 	"
 done
 
