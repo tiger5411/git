5ed9caffe6bde1ca53dec216f10986de80d8a744
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-04-21 19:36:50 +0000

    grep engine wip

--- t/perf/p7821-grep-engines-fixed.sh
+++ t/perf/p7821-grep-engines-fixed.sh
@@ -9,20 +9,28 @@ test_checkout_worktree
 
 for engine in fixed basic extended perl
 do
-	# These patterns gradully go from matching just a few lines to
-	# matching a *lot* in linux.git
-	for pattern in \
-		'pedantic' \
-		'TODO' \
-		'bug' \
-		'and' \
-		'int' \
-		' '
-	do
-		test_perf "$engine with $pattern" "
-			git -c grep.patternType=$engine grep -- '$pattern' || :
-		"
-	done
+	test_perf "$engine grep int"    "
+		git -c grep.patternType=$engine grep int >out.$engine.int || :
+	"
+	test_perf "$engine grep -i int" "
+		git -c grep.patternType=$engine grep -i int >out.$engine.i.int || :
+	"
+	test_perf "$engine grep æ"      "
+		git -c grep.patternType=$engine grep æ >out.$engine.æ || :
+	"
+	test_perf "$engine grep -i æ"      "
+		git -c grep.patternType=$engine grep -i æ >out.$engine.i.æ || :
+	"
+done
+
+for suffix in int i.int æ i.æ
+do  
+	test_expect_success "assert that the $suffix patterns found the same things under all engines" "
+		test_cmp out.fixed.$suffix out.basic.$suffix &&
+		test_cmp out.fixed.$suffix out.extended.$suffix &&
+		test_cmp out.fixed.$suffix out.extended.$suffix &&
+		test_cmp out.fixed.$suffix out.perl.$suffix
+	"
 done
 
 test_done
