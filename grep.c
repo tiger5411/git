f7b282122d35f74a4dc98cb1be578d05fc779a92
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-04-22 11:47:19 +0000

    this solves a bunch of todo tests

--- grep.c
+++ grep.c
@@ -547,12 +547,6 @@ static int is_fixed(const char *s, size_t len)
 {
 	size_t i;
 
-	/* regcomp cannot accept patterns with NULs so we
-	 * consider any pattern containing a NUL fixed.
-	 */
-	if (memchr(s, 0, len))
-		return 1;
-
 	for (i = 0; i < len; i++) {
 		if (is_regex_special(s[i]))
 			return 0;
@@ -606,7 +600,19 @@ static void compile_regexp(struct grep_pat *p, struct grep_opt *opt)
 	 * want to use kws.
 	 */
 
-	if (opt->fixed || is_fixed(p->pattern, p->patternlen))
+#ifdef USE_LIBPCRE2
+	if (!icase && ascii_only && is_fixed(p->pattern, p->patternlen)) {
+		compile_pcre2_pattern(p, opt);
+		return;
+	}
+
+	if (memchr(p->pattern, 0, p->patternlen) && !icase && ascii_only) {
+		compile_pcre2_pattern(p, opt);
+		return;
+	}
+#endif
+	
+	if (opt->fixed || is_fixed(p->pattern, p->patternlen) || memchr(p->pattern, 0, p->patternlen))
 		p->fixed = !icase || ascii_only;
 	else
 		p->fixed = 0;
