1f6fe331ea68fb635fb93314b98df3f9b9edadff
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-08 21:08:56 +0000

    grep: factor test for \0 in grep patterns into a function
    Factor the test for \0 in grep patterns into a function. Since commit
    9eceddeec6 ("Use kwset in grep", 2011-08-21) any pattern containing a
    \0 is considered fixed as regcomp() can't handle it.
    
    This limitation was never documented, and other some regular
    expression engines are capable of compiling a pattern containing a
    \0. Factoring this out makes a subsequent change which does that
    smaller.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- grep.c
+++ grep.c
@@ -545,18 +545,22 @@ static int is_fixed(const char *s, size_t len)
 {
 	size_t i;
 
-	/* regcomp cannot accept patterns with NULs so we
-	 * consider any pattern containing a NUL fixed.
-	 */
-	if (memchr(s, 0, len))
-		return 1;
-
 	for (i = 0; i < len; i++) {
 		if (is_regex_special(s[i]))
 			return 0;
 	}
 
 	return 1;
+}
+
+static int has_null(const char *s, size_t len)
+{
+	/* regcomp cannot accept patterns with NULs so when using it
+	 * we consider any pattern containing a NUL fixed.
+	 */
+	if (memchr(s, 0, len))
+		return 1;
+	return 0;
 }
 
 static void compile_fixed_regexp(struct grep_pat *p, struct grep_opt *opt)
@@ -602,7 +606,7 @@ static void compile_regexp(struct grep_pat *p, struct grep_opt *opt)
 	 * simple string match using kws.  p->fixed tells us if we
 	 * want to use kws.
 	 */
-	if (opt->fixed || is_fixed(p->pattern, p->patternlen))
+	if (opt->fixed || has_null(p->pattern, p->patternlen) || is_fixed(p->pattern, p->patternlen))
 		p->fixed = !icase || ascii_only;
 	else
 		p->fixed = 0;
