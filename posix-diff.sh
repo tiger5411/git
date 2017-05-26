2c71c78a7d01bd06e855c1f25d6ff3619399ba82
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-26 22:35:34 +0000

    bump with diff files

--- posix-diff.sh
+++ posix-diff.sh
@@ -5,10 +5,10 @@ parallel -k '
 		"$(printf \\$(printf %03o {1}))" \
 		"$(printf \\$(printf %03o {2}))" \
 	>/tmp/f-{1}-{2}
-	./git --exec-path=$(pwd) -C ../pcre grep -G -I -f /tmp/f-{1}-{2} \
+	./git --exec-path=$(pwd) -C ../pcre grep -E -I -f /tmp/f-{1}-{2} \
 		 >/tmp/o-{1}-{2}.pcre \
 		2>/tmp/e-{1}-{2}.pcre
-	USE_REGCOMP=1 ./git --exec-path=$(pwd) -C ../pcre grep -G -I -f /tmp/f-{1}-{2} \
+	USE_REGCOMP=1 ./git --exec-path=$(pwd) -C ../pcre grep -E -I -f /tmp/f-{1}-{2} \
 		 >/tmp/o-{1}-{2}.regcomp \
 		2>/tmp/e-{1}-{2}.regcomp
 	if ! diff -ru /tmp/o-{1}-{2}.regcomp /tmp/o-{1}-{2}.pcre >/tmp/d-{1}-{2}
