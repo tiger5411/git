6ec5dd71a0509fd76a769c22cfc9c8a059dd4a23
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-05-26 22:20:22 +0000

    moar diff

--- posix-diff.sh
+++ posix-diff.sh
@@ -5,12 +5,18 @@ parallel -k '
 		"$(printf \\$(printf %03o {1}))" \
 		"$(printf \\$(printf %03o {2}))" \
 	>/tmp/f-{1}-{2}
-	./git-grep               -G -I -f /tmp/f-{1}-{2} \
-		 >/tmp/o-{1}-{2}.pcre.basic \
-		2>/tmp/e-{1}-{2}.pcre.basic
-	USE_REGCOMP=1 ./git-grep -G -I -f /tmp/f-{1}-{2} \
-		 >/tmp/o-{1}-{2}.regcomp.basic \
-		2>/tmp/e-{1}-{2}.regcomp.basic
-	diff -ru /tmp/o-{1}-{2}.regcomp.basic /tmp/o-{1}-{2}.pcre.basic | head -n 10
-	rm /tmp/f-{1}-{2} /tmp/o-{1}-{2}.pcre.basic /tmp/o-{1}-{2}.regcomp.basic
-' ::: {33..58} ::: {33..58}
+	./git --exec-path=$(pwd) -C ../pcre grep -G -I -f /tmp/f-{1}-{2} \
+		 >/tmp/o-{1}-{2}.pcre \
+		2>/tmp/e-{1}-{2}.pcre
+	USE_REGCOMP=1 ./git --exec-path=$(pwd) -C ../pcre grep -G -I -f /tmp/f-{1}-{2} \
+		 >/tmp/o-{1}-{2}.regcomp \
+		2>/tmp/e-{1}-{2}.regcomp
+	if ! diff -ru /tmp/o-{1}-{2}.regcomp /tmp/o-{1}-{2}.pcre >/tmp/d-{1}-{2}
+	then
+		printf "DIFF: %s+%s (%s)\n" {1} {2} $(cat /tmp/f-{1}-{2})
+		head -n 10 /tmp/d-{1}-{2}
+	else
+		printf "SAME: %s+%s\n" {1} {2}
+	fi
+	rm /tmp/f-{1}-{2} /tmp/o-{1}-{2}.pcre /tmp/o-{1}-{2}.regcomp /tmp/d-{1}-{2}
+' ::: {1..127} ::: {1..127}
