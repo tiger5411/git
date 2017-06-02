ad7d562e67dabd553281662c9e16803cc6688b83
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2017-06-02 23:52:43 +0200

    add perf test of pcre codepath

---
+++ t/perf/p0101-globbing-wildmatch.sh
@@ -0,0 +1,109 @@
+#!/bin/sh
+
+test_description="Tests wildmatch()"
+
+. ./perf-lib.sh
+
+test_perf_large_repo
+
+for pat in \
+	'foo' \
+	'bar' \
+	'' \
+	'???' \
+	'??' \
+	'*' \
+	'f*' \
+	'*f' \
+	'*foo*' \
+	'*ob*a*r*' \
+	'*ab' \
+	'foo\*' \
+	'foo\*bar' \
+	'f\\oo' \
+	'*[al]?' \
+	'[ten]' \
+	'**[!te]' \
+	'**[!ten]' \
+	't[a-g]n' \
+	't[!a-g]n' \
+	't[!a-g]n' \
+	't[^a-g]n' \
+	'a[]]b' \
+	'a[]-]b' \
+	'a[]-]b' \
+	'a[]-]b' \
+	'a[]a-]b' \
+	']' ']' \
+	'foo*bar' \
+	'foo**bar' \
+	'foo**bar' \
+	'foo/**/bar' \
+	'foo/**/**/bar' \
+	'foo/**/bar' \
+	'foo/**/**/bar' \
+	'foo/**/bar' \
+	'foo/**/**/bar' \
+	'foo?bar' \
+	'foo[/]bar' \
+	'f[^eiu][^eiu][^eiu][^eiu][^eiu]r' \
+	'f[^eiu][^eiu][^eiu][^eiu][^eiu]r' \
+	'**/foo' \
+	'**/foo' \
+	'**/foo' \
+	'*/foo' \
+	'**/bar*' \
+	'**/bar/*' \
+	'**/bar/*' \
+	'**/bar/**' \
+	'**/bar/*' \
+	'**/bar/**' \
+	'**/bar**' \
+	'*/bar/**' \
+	'*/bar/**' \
+	'**/bar/*/*' \
+	'a[c-c]st' \
+	'a[c-c]rt' \
+	'[!]-]' \
+	'[!]-]' \
+	'\' \
+	'\' \
+	'*/\' \
+	'*/\\' \
+	'foo' \
+	'@foo' \
+	'@foo' \
+	'\[ab]' \
+	'[[]ab]' \
+	'[[:]ab]' \
+	'[[::]ab]' \
+	'[[:digit]ab]' \
+	'[\[:]ab]' \
+	'\??\?b' \
+	'\a\b\c' \
+	'' \
+	'**/t[o]' \
+	'[[:alpha:]][[:digit:]][[:upper:]]' \
+	'[[:digit:][:upper:][:space:]]' \
+	'[[:digit:][:upper:][:space:]]' \
+	'[[:digit:][:upper:][:space:]]' \
+	'[[:digit:][:upper:][:spaci:]]' \
+	'[[:digit:][:upper:][:space:]]' \
+	'[[:digit:][:upper:][:space:]]' \
+	'[[:digit:][:punct:][:space:]]' \
+	'[[:xdigit:]]' \
+	'[[:xdigit:]]' \
+	'[[:xdigit:]]' \
+	'[[:alnum:][:alpha:][:blank:][:cntrl:][:digit:][:graph:][:lower:][:print:][:punct:][:space:][:upper:][:xdigit:]]' \
+	'[^[:alnum:][:alpha:][:blank:][:cntrl:][:digit:][:lower:][:space:][:upper:][:xdigit:]]' \
+	'[a-c[:digit:]x-z]' \
+	'[a-c[:digit:]x-z]' \
+	'[a-c[:digit:]x-z]' \
+	'[a-c[:digit:]x-z]'
+do
+	test_perf "wildmatch($pat)" "
+		git ls-files '$pat' >/dev/null || :
+	"
+done
+
+test_done
