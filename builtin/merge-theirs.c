b44834de43aae1ab840230478f5864d69f3631b8
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-04-18 22:20:38 +0000

    bump

--- builtin/merge-theirs.c
+++ builtin/merge-theirs.c
@@ -6,10 +6,23 @@
  */
 #include "git-compat-util.h"
 #include "builtin.h"
-#include "diff.h"
+#include "run-command.h"
 
 static const char builtin_merge_theirs_usage[] =
 	"git merge-theirs <base>... -- HEAD <remote>...";
+
+static void read_tree_hex_oid(const char *hex_oid)
+{
+	int i = 0;
+	const char *args[4];
+
+	args[i++] = "read-tree";
+	args[i++] = hex_oid;
+	args[i] = NULL;
+
+	if (run_command_v_opt(args, RUN_GIT_CMD))
+		die(_("read-tree failed"));
+}
 
 int cmd_merge_theirs(int argc, const char **argv, const char *prefix)
 {
@@ -43,7 +56,8 @@ int cmd_merge_theirs(int argc, const char **argv, const char *prefix)
 	if (get_oid(branch, &commit))
 		die(_("could not resolve ref '%s'"), branch);
 
-	fprintf(stderr, "Converted %s to %d. So we select %s with %d\n", mainline_str, mainline, argv[argc_offset + mainline], argc);
+	/* Read the Nth tree */
+	read_tree_hex_oid(oid_to_hex(&commit));
 
 	return 0;
 }
