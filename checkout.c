18f13ecd89ee09188a42fb13f68a2545058cc187
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-05-02 10:17:01 +0000

    checkout & worktree: introduce a core.DWIMRemote setting
    Introduce a core.DWIMRemote setting which can be used to designate a
    remote to prefer (via core.DWIMRemote=origin) when running e.g. "git
    checkout master" to mean origin/master, even though there's other
    remotes that have the "master" branch.
    
    I want this because it's very handy to use this workflow to checkout a
    repository and create a topic branch, then get back to a "master" as
    retrieved from upstream:
    
        (
            rm -rf /tmp/tbdiff &&
            git clone git@github.com:trast/tbdiff.git &&
            cd tbdiff &&
            git branch -m topic &&
            git checkout master
        )
    
    That will output:
    
        Branch 'master' set up to track remote branch 'master' from 'origin'.
        Switched to a new branch 'master'
    
    But as soon as a new remote is added (e.g. just to inspect something
    from someone else) the DWIMery goes away:
    
        (
            rm -rf /tmp/tbdiff &&
            git clone git@github.com:trast/tbdiff.git &&
            cd tbdiff &&
            git branch -m topic &&
            git remote add avar git@github.com:avar/tbdiff.git &&
            git fetch avar &&
            git checkout master
        )
    
    Will output:
    
        error: pathspec 'master' did not match any file(s) known to git.
    
    The new core.DWIMRemote config allows me to say that whenever that
    ambiguity comes up I'd like to prefer "origin", and it'll still work
    as though the only remote I had was "origin".
    
    I considered calling the config setting checkout.DWIMRemote, but then
    discovered that this behavior is also used by "worktree" for similar
    purposes, so it makes sense to have it under core.*. As noted in the
    documentation we may also want to use this in other commands in the
    future if they have similar DWIM behavior in the presence of one
    remote.
    
    See also 70c9ac2f19 ("DWIM "git checkout frotz" to "git checkout -b
    frotz origin/frotz"", 2009-10-18) which introduced this DWIM feature
    to begin with, and 4e85333197 ("worktree: make add <path> <branch>
    dwim", 2017-11-26) which added it to git-worktree.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- checkout.c
+++ checkout.c
@@ -1,12 +1,15 @@
 #include "cache.h"
 #include "remote.h"
 #include "checkout.h"
+#include "config.h"
 
 struct tracking_name_data {
 	/* const */ char *src_ref;
 	char *dst_ref;
 	struct object_id *dst_oid;
 	int unique;
+	const char *dwim_remote;
+	char *dwim_dst_ref;
 };
 
 static int check_tracking_name(struct remote *remote, void *cb_data)
@@ -20,6 +23,8 @@ static int check_tracking_name(struct remote *remote, void *cb_data)
 		free(query.dst);
 		return 0;
 	}
+	if (cb->dwim_remote && !strcmp(remote->name, cb->dwim_remote))
+		cb->dwim_dst_ref = xstrdup(query.dst);
 	if (cb->dst_ref) {
 		free(query.dst);
 		cb->unique = 0;
@@ -31,13 +36,21 @@ static int check_tracking_name(struct remote *remote, void *cb_data)
 
 const char *unique_tracking_name(const char *name, struct object_id *oid)
 {
-	struct tracking_name_data cb_data = { NULL, NULL, NULL, 1 };
+	const char *dwim_remote = NULL;
+	struct tracking_name_data cb_data = { NULL, NULL, NULL, 1, NULL, NULL };
+	if (!git_config_get_string_const("core.dwimremote", &dwim_remote))
+		cb_data.dwim_remote = dwim_remote;
 	cb_data.src_ref = xstrfmt("refs/heads/%s", name);
 	cb_data.dst_oid = oid;
 	for_each_remote(check_tracking_name, &cb_data);
 	free(cb_data.src_ref);
-	if (cb_data.unique)
+	free((char *)dwim_remote);
+	if (cb_data.unique) {
+		free(cb_data.dwim_dst_ref);
 		return cb_data.dst_ref;
+	}
 	free(cb_data.dst_ref);
+	if (cb_data.dwim_dst_ref)
+		return cb_data.dwim_dst_ref;
 	return NULL;
 }
