9259f7c269e354bbe750583a3fc9d2f3d7a23ab3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2018-06-06 08:27:31 +0000

    config.c: use braces on multiple conditional arms
    Adjust this code that'll be modified in a subsequent change per the
    CodingGuidelines.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- config.c
+++ config.c
@@ -1149,9 +1149,9 @@ static int git_default_core_config(const char *var, const char *value)
 	if (!strcmp(var, "core.abbrev")) {
 		if (!value)
 			return config_error_nonbool(var);
-		if (!strcasecmp(value, "auto"))
+		if (!strcasecmp(value, "auto")) {
 			default_abbrev = -1;
-		else {
+		} else {
 			int abbrev = git_config_int(var, value);
 			if (abbrev < minimum_abbrev || abbrev > 40)
 				return error("abbrev length out of range: %d", abbrev);
