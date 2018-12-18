a7936f1d200df474454a78f389b01f400da4a0ad
Christian Couder <chriscool@tuxfamily.org>
2018-12-18 13:40:25 +0100

    reftable: declare struct ref_update

--- refs/reftable.h
+++ refs/reftable.h
@@ -2,6 +2,7 @@
 #define REFTABLE_H
 
 struct reftable_header;
+struct ref_update;
 
 void reftable_header_init(struct reftable_header *header, uint32_t block_size,
 			  uint64_t min_update_index, uint64_t max_update_index);
