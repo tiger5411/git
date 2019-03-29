aa5752496733a342a17120692bdd9f73f4dd8fa2
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2019-03-29 22:30:05 +0100

    trace2: move duplicate tr2_tbuf code into static function
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- trace2/tr2_tbuf.c
+++ trace2/tr2_tbuf.c
@@ -15,7 +15,7 @@ void tr2_tbuf_local_time(struct tr2_tbuf *tb)
 		  tm.tm_min, tm.tm_sec, (long)tv.tv_usec);
 }
 
-void tr2_tbuf_utc_datetime_extended(struct tr2_tbuf *tb)
+static void tr2_tbuf_utc_datetime_fmt(struct tr2_tbuf *tb, const char *fmt)
 {
 	struct timeval tv;
 	struct tm tm;
@@ -26,23 +26,17 @@ void tr2_tbuf_utc_datetime_extended(struct tr2_tbuf *tb)
 	gmtime_r(&secs, &tm);
 
 	xsnprintf(tb->buf, sizeof(tb->buf),
-		  "%4d-%02d-%02dT%02d:%02d:%02d.%06ldZ", tm.tm_year + 1900,
+		  fmt, tm.tm_year + 1900,
 		  tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,
 		  (long)tv.tv_usec);
 }
 
+void tr2_tbuf_utc_datetime_extended(struct tr2_tbuf *tb)
+{
+	tr2_tbuf_utc_datetime_fmt(tb, "%4d-%02d-%02dT%02d:%02d:%02d.%06ldZ");
+}
+
 void tr2_tbuf_utc_datetime_for_filename(struct tr2_tbuf *tb)
 {
-	struct timeval tv;
-	struct tm tm;
-	time_t secs;
-
-	gettimeofday(&tv, NULL);
-	secs = tv.tv_sec;
-	gmtime_r(&secs, &tm);
-
-	xsnprintf(tb->buf, sizeof(tb->buf),
-		  "%4d%02d%02d-%02d%02d%02d-%06ld", tm.tm_year + 1900,
-		  tm.tm_mon + 1, tm.tm_mday, tm.tm_hour, tm.tm_min, tm.tm_sec,
-		  (long)tv.tv_usec);
+	tr2_tbuf_utc_datetime_fmt(tb, "%4d%02d%02d-%02d%02d%02d-%06ld");
 }
