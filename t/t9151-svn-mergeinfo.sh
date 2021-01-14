0e77779947d778d2612d7121aff59e59a4669a89
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-01-14 18:03:56 +0100

    tests: use "test_might_fail ok=sigpipe grep" when appropriate
    Change tests where the "grep" might exit with non-zero to use our new
    "test_might_fail ok=sigpipe" wrapper. A subsequent commit will show
    how useful this is in combination with "set -o pipefail".
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/t9151-svn-mergeinfo.sh
+++ t/t9151-svn-mergeinfo.sh
@@ -18,19 +18,19 @@ test_expect_success 'load svn dump' "
 
 test_expect_success 'all svn merges became git merge commits' '
 	unmarked=$(git rev-list --parents --all --grep=Merge |
-		grep -v " .* " | cut -f1 -d" ") &&
+		test_might_fail ok=sigpipe grep -v " .* " | cut -f1 -d" ") &&
 	[ -z "$unmarked" ]
 	'
 
 test_expect_success 'cherry picks did not become git merge commits' '
 	bad_cherries=$(git rev-list --parents --all --grep=Cherry |
-		grep " .* " | cut -f1 -d" ") &&
+		test_might_fail ok=sigpipe grep grep " .* " | cut -f1 -d" ") &&
 	[ -z "$bad_cherries" ]
 	'
 
 test_expect_success 'svn non-merge merge commits did not become git merge commits' '
 	bad_non_merges=$(git rev-list --parents --all --grep=non-merge |
-		grep " .* " | cut -f1 -d" ") &&
+		test_might_fail ok=sigpipe grep grep " .* " | cut -f1 -d" ") &&
 	[ -z "$bad_non_merges" ]
 	'
 
