a3b09ed88c8d8865b807be77d730e74d27293912
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-01-24 01:40:15 +0100

    grep/pcre2: simplify boolean spaghetti
    Simplify an expression I added in 870eea8166 (grep: do not enter
    PCRE2_UTF mode on fixed matching, 2019-07-26) by using a simple
    application of De Morgan's laws[1]. I.e.:
    
        NOT(A && B) is Equivalent to (NOT(A) OR NOT(B))
    
    1. https://en.wikipedia.org/wiki/De_Morgan%27s_laws
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- grep.c
+++ grep.c
@@ -491,7 +491,7 @@ static void compile_pcre2_pattern(struct grep_pat *p, const struct grep_opt *opt
 		options |= PCRE2_CASELESS;
 	}
 	if (!opt->ignore_locale && is_utf8_locale() && has_non_ascii(p->pattern) &&
-	    !(!opt->ignore_case && (p->fixed || p->is_fixed)))
+	    (opt->ignore_case || !(p->fixed || p->is_fixed)))
 		options |= PCRE2_UTF;
 
 	p->pcre2_pattern = pcre2_compile((PCRE2_SPTR)p->pattern,
