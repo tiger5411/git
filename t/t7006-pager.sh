cba284dcf55d4bf15e4c8690a4df76b0d8d54840
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-02-01 14:48:23 +0100

    pager: test for exit code
    Add tests for how git behaves when the pager itself exits with
    non-zero, as well as for us exiting with 141 when we're killed with
    SIGPIPE due to the pager not consuming its output.
    
    There is some recent discussion[1] about these semantics, but aside
    from what we want to do in the future, we should have a test for the
    current behavior.
    
    This test construct is stolen from 7559a1be8a0 (unblock and unignore
    SIGPIPE, 2014-09-18).
    
    1. https://lore.kernel.org/git/87o8h4omqa.fsf@evledraar.gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/t7006-pager.sh
+++ t/t7006-pager.sh
@@ -656,4 +656,31 @@ test_expect_success TTY 'git tag with auto-columns ' '
 	test_cmp expect actual
 '
 
+test_expect_success TTY,!MINGW 'git returns SIGPIPE on early pager exit' '
+	test_when_finished "rm pager-used" &&
+	test_config core.pager ">pager-used; head -n 1; exit 0" &&
+
+	OUT=$( ((test_terminal git log; echo $? 1>&3) | :) 3>&1 ) &&
+	test_match_signal 13 "$OUT" &&
+	test_path_is_file pager-used
+'
+
+test_expect_success TTY,!MINGW 'git returns SIGPIPE on early pager non-zero exit' '
+	test_when_finished "rm pager-used" &&
+	test_config core.pager ">pager-used; head -n 1; exit 1" &&
+
+	OUT=$( ((test_terminal git log; echo $? 1>&3) | :) 3>&1 ) &&
+	test_match_signal 13 "$OUT" &&
+	test_path_is_file pager-used
+'
+
+test_expect_success TTY,!MINGW 'git discards pager non-zero exit' '
+	test_when_finished "rm pager-used" &&
+	test_config core.pager "wc >pager-used; exit 1" &&
+
+	OUT=$( ((test_terminal git log; echo $? 1>&3) | :) 3>&1 ) &&
+	test "$OUT" -eq 0 &&
+	test_path_is_file pager-used
+'
+
 test_done
