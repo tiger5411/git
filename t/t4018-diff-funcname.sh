219043a4881d84af340ffe4a61a13272bf839538
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-02-14 19:01:59 +0100

    userdiff tests: list builtin drivers via test-tool
    Change the userdiff test to list the builtin drivers via the
    test-tool, using the new for_each_userdiff_driver() API function.
    
    This gets rid of the need to modify this part of the test every time a
    new pattern is added, see 2ff6c34612 (userdiff: support Bash,
    2020-10-22) and 09dad9256a (userdiff: support Markdown, 2020-05-02)
    for two recent examples.
    
    I only need the "list-builtin-drivers "argument here, but let's add
    "list-custom-drivers" and "list-drivers" too, just because it's easy.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/t4018-diff-funcname.sh
+++ t/t4018-diff-funcname.sh
@@ -8,6 +8,9 @@ test_description='Test custom diff function name patterns'
 . ./test-lib.sh
 
 test_expect_success 'setup' '
+	builtin_drivers=$(test-tool userdiff list-builtin-drivers) &&
+	test -n "$builtin_drivers" &&
+
 	# a non-trivial custom pattern
 	git config diff.custom1.funcname "!static
 !String
@@ -26,30 +29,7 @@ test_expect_success 'setup' '
 '
 
 diffpatterns="
-	ada
-	bash
-	bibtex
-	cpp
-	csharp
-	css
-	dts
-	elixir
-	fortran
-	fountain
-	golang
-	html
-	java
-	markdown
-	matlab
-	objc
-	pascal
-	perl
-	php
-	python
-	ruby
-	rust
-	tex
-	default
+	$builtin_drivers
 	custom1
 	custom2
 	custom3
