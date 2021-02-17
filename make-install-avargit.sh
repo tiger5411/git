955f2bc6788b7d960fed17e951f0e65c462956b3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-02-17 23:55:17 +0100

    Also install it

--- make-install-avargit.sh
+++ make-install-avargit.sh
@@ -7,6 +7,7 @@ git checkout master
 git reset --hard @{u}
 
 git merge \
+    --no-edit \
     avar/test-lib-misc-fixes-2 \
     avar/log-pager-exit-status \
     avar/diff-free-2 \
@@ -16,3 +17,11 @@ git merge \
     avar/pcre2-fixes-diffcore-pickaxe-pcre-etc-2-on-master \
     avar/commit-graph-usage \
     avar/diff-W-context
+
+time make -j $(parallel --number-of-cores) \
+     USE_LIBPCRE=Y \
+     LIBPCREDIR=$HOME/g/pcre2/inst \
+     CFLAGS="-O0 -g" \
+     DEVELOPER=1 \
+     prefix=/home/avar/local \
+     all man install install-man
