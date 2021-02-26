e43417d04e06113711a6330cde4c85890dcfaad4
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-02-26 14:07:46 +0100

    test-lib: indent and format GIT_TEST_TEE_OUTPUT_FILE code
    Change the indentation and formatting of the GIT_TEST_TEE_OUTPUT_FILE
    code block added in 44138559e8 (test-lib.sh: optionally output to
    test-results/$TEST.out, too, 2009-02-04).
    
    This brings it in line with the general style in our test library, and
    makes it easier to read modifications of it in subsequent commits.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/test-lib.sh
+++ t/test-lib.sh
@@ -377,8 +377,10 @@ then
 	# from any previous runs.
 	>"$GIT_TEST_TEE_OUTPUT_FILE"
 
-	(GIT_TEST_TEE_STARTED=done ${TEST_SHELL_PATH} "$0" "$@" 2>&1;
-	 echo $? >"$TEST_RESULTS_BASE.exit") | tee -a "$GIT_TEST_TEE_OUTPUT_FILE"
+	(
+		GIT_TEST_TEE_STARTED=done ${TEST_SHELL_PATH} "$0" "$@" 2>&1
+		echo $? >"$TEST_RESULTS_BASE.exit"
+	) | tee -a "$GIT_TEST_TEE_OUTPUT_FILE"
 	test "$(cat "$TEST_RESULTS_BASE.exit")" = 0
 	exit
 fi
