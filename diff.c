e5df57c34405f818b429194c9d3b95da1b6f7c30
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-04 18:19:46 +0100

    diff.c: remove redundant canon_mode() call
    Remove a call to canon_mode() from fill_filespec(). This has been
    redundant since the tree-walk.c API supplies it pre-canonicalized
    since 7146e66f086 (tree-walk: finally switch over tree descriptors to
    contain a pre-parsed entry, 2014-02-06).
    
    This call to the predecessor of canon_mode() was added back in
    4130b995719 ([PATCH] Diff updates to express type changes,
    2005-05-26).
    
    This was the only such call in the codebase. The rest are all either
    one of these sorts of forms:
    
        canon_mode(st.st_mode); /* a stat(2) struct */
        canon_mode(S_IFREG | 0644) /* A compile-time literal */
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- diff.c
+++ diff.c
@@ -3846,7 +3846,7 @@ void fill_filespec(struct diff_filespec *spec, const struct object_id *oid,
 		   int oid_valid, unsigned short mode)
 {
 	if (mode) {
-		spec->mode = canon_mode(mode);
+		spec->mode = mode;
 		oidcpy(&spec->oid, oid);
 		spec->oid_valid = oid_valid;
 	}
