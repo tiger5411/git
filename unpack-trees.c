9d049fdbd00c56aee02a4dbd0d523718d2db20a0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-04 13:30:37 +0100

    tree-walk.h API: move canon_mode() back out of decode_tree_entry()
    Move the canon_mode() call back out of decode_tree_entry(), and
    instead make it the responsibility of its callers to canonicalize the
    tree modes we get.
    
    This effectively reverts 7146e66f086 (tree-walk: finally switch over
    tree descriptors to contain a pre-parsed entry, 2014-02-06), with the
    recent of most callers away from "mode" (now "raw_mode") towards "enum
    object_id" in recent commit the motivation for that commit effectively
    doesn't exist anymore.
    
    I.e. I'm not adding the canon_mode() call back to
    tree_entry_extract(), instead it's now become sane to move this
    responsibility to those callers that still care about the "raw_mode".
    
    That change was meant as a pure optimization change, but it actually
    introduced a subtle bug. We were left without any low-level API to get
    non-standard mode bits out of trees. Having non-standard modes isn't
    the norm, and fsck should warn about it.
    
    Except after 7146e66f086 it couldn't anymore, since the modes
    fsck_tree() got would be pre-sanitized for it. I believe that fsck
    issue is per-se a serious bug, the "bad mode" was a default warning,
    not an error.
    
    This change makes that fsck check work again, why aren't there any
    test changes for fsck here? Because we didn't have a test for that
    fsck feature in the first place, which is why the regression in
    7146e66f086 snuck by us. A follow-up commit will add such a test.
    
    It is possible that this commit is introducing some subtle regression
    that I've missed.
    
    We are now propagating the "raw_mode" outside of everything downstream
    of decode_tree_entry(), which is everything we have that decodes
    trees. It's our most low-level tree decoding API.
    
    As shown here we rely parsing out a "raw" (and possibly something fsck
    would complain about) mode as-is, but when we run merge, add something
    new to the index, create an archive etc. we don't want to propagate
    that bad mode when we create new data. We want to canon_mode() it.
    
    I'm also pretty sure that we don't have good enough test coverage for
    those scenarios. We barely have tests for these bad mode bits at
    all (not even one for fsck). We definitely are not testing all
    merge/index/archive etc. interactions.
    
    Still, I think this change is worth it overall, because:
    
     1. We must have a way to get at these raw modes in some way, even if
        just for fsck. There's also other things that care, see e.g. the
        FIXME comment in 62fdec17a11 (merge-ort: flesh out implementation of
        handle_content_merge(), 2021-01-01)
    
     2. #1 is not a justification for this change, I could have e.g. just
        added the ability to pass some "want_raw" flag into
        decode_tree_entry() for use in fsck. But I think with the migration
        of most tree iteration towards "enum object_type" it's become worth
        it.
    
     3. Yes our test coverage sucks, but before 7146e66f086 we were also
        spreading what's now the "raw_mode" all over the place. That commit
        was first released with Git v2.0.0 in mid-2014. A while ago for sure,
        but most of this code existed in something approximating its current
        form then. This isn't new territory.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- unpack-trees.c
+++ unpack-trees.c
@@ -868,6 +868,7 @@ static int traverse_trees_recursive(int n, unsigned long dirmask,
 	newinfo.name = p->path;
 	newinfo.namelen = p->pathlen;
 	newinfo.mode = p->raw_mode;
+	newinfo.mode = canon_mode(newinfo.mode);
 	newinfo.pathlen = st_add3(newinfo.pathlen, tree_entry_len(p), 1);
 	newinfo.df_conflicts |= df_conflicts;
 
@@ -1020,7 +1021,8 @@ static struct cache_entry *create_ce_entry(const struct traverse_info *info,
 		is_transient ?
 		make_empty_transient_cache_entry(len) :
 		make_empty_cache_entry(istate, len);
-	unsigned int mode = n->raw_mode;
+	unsigned int mode = canon_mode(n->raw_mode);
+	mode = canon_mode(mode);
 
 	ce->ce_mode = create_ce_mode(mode);
 	ce->ce_flags = create_ce_flags(stage);
