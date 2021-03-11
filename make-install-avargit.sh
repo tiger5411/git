4df2972f3e845eb304fcb4ad1b4b5b613f72f7fc
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-11 16:53:08 +0100

    start tagging my built versions

--- make-install-avargit.sh
+++ make-install-avargit.sh
@@ -2,7 +2,38 @@
 set -e
 set -x
 
+meta_dir="$(pwd)"
 cd ~/g/git.build
+
+tag_name() {
+	date +'avargit-v%Y-%m-%d-%H%M%S'
+}
+
+tag_it() {
+        tmp=$(mktemp /tmp/avargit-XXXXX)
+	cat >$tmp <<-EOF
+	object $1
+	type commit
+	tag $2
+	tagger $(git config user.name) <$(git config user.email)> $(date "+%s %z")
+
+	My personal git version built from (mostly) my outstanding topics.
+
+	The hacky script that built this follows after the NUL:
+	EOF
+
+	printf "\0" >>$tmp
+	cat "$meta_dir"/"$0" >>$tmp
+
+        git mktag --strict <$tmp
+}
+
+show_built_from() {
+        built_from=$(git version --build-options | grep -P -o '(?<=built from commit: ).*')
+        echo "Info:"
+        echo "  - Now running: $(git version)"
+        echo "  - Now running built from: $(git reference $built_from)"
+}
 
 reset_it() {
         git reset --hard @{u}
@@ -10,12 +41,14 @@ reset_it() {
         git reset --hard @{u}
 }
 
+show_built_from
 reset_it
 git checkout build-master || git checkout -b build-master -t origin/master
 
 # TODO:
 #   make-dot-not-HEAD-warn-3
 #   avar/object-api-to-no-string-argument (TODO: handle -t "some garbage" case)
+#   unconditional-abbrev-redo-2021-rebase
 # Ejected:
 #   avar/fix-tree-mode-fsck (in favor of avar/tree-walk-api-refactor)
 #
@@ -42,7 +75,8 @@ for series in \
     avar/makefile-rename-git-binary-not-in-place \
     avar/mktag-broken-and-chain-typo \
     avar/support-test-verbose-under-prove-2 \
-    avar/sh-remove-sha1-variables
+    avar/sh-remove-sha1-variables \
+    avar/test-lib-bail-out-on-accidental-prove-invocation
 do
 	git merge --no-edit $series || EDITOR=cat git merge --continue
 done
@@ -57,10 +91,30 @@ make_it() {
                 $@
 }
 
+# First run a smaller subset of tests, likelier to have failures:
 git diff --diff-filter=ACMR --name-only --relative=t/ -p @{u}.. -- t/t[0-9]*.sh >/tmp/git.build-tests
+
+# Compile
 make_it all man
+
+# Run all tests
 (cd t && prove -j $(nproc) $(cat /tmp/git.build-tests))
+(cd t && GIT_TEST_DEFAULT_HASH=sha256 prove -j $(nproc) t[0-9]*.sh)
+
+# Install it
+new_version=$(git rev-parse HEAD)
+new_tagname=$(tag_name)
+new_tag=$(tag_it "$new_version" "$new_tagname")
+last_version=$(git rev-parse avar/private)
 make_it install install-man
-(cd t && prove -j $(nproc) t[0-9]*.sh)
+show_built_from
+
+# Post-install & report
+echo "Range-diff between last built and what I've got now:"
+git --no-pager range-diff --left-only avar/private...
+
+echo "Shortlog from @{u}..:"
 git --no-pager shortlog @{u}..
+
 git push avar HEAD:private -f
+git push avar $new_tag:refs/built-tags/$new_tagname
