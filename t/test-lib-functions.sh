0cb6cae215250cb5ef9a9f6605e5813808e92f19
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-22 00:40:47 +0100

    better default

--- t/test-lib-functions.sh
+++ t/test-lib-functions.sh
@@ -1234,8 +1234,9 @@ test_create_repo () {
 		"${GIT_TEST_INSTALLED:-$GIT_EXEC_PATH}/git$X" -c \
 			init.defaultBranch="${GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME-master}" \
 			init \
-			--no-template >&3 2>&4 ||
+			"--template=$GIT_BUILD_DIR/templates/blt/" >&3 2>&4 ||
 		error "cannot run git init -- have you built things yet?"
+		mv .git/hooks .git/hooks-disabled
 	) || exit
 }
 
