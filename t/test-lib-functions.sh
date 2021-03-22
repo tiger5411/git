424caad189f70d2b37424aac5baad32457f440ad
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-22 23:27:34 +0100

    test-lib: modernize test_create_repo() function
    Remove redundant "mkdir -p", argument number checking', test
    environment sanity checking, and disabling of hooks from
    test_create_repo(). As we'll see below these were all either redundant
    to other test other framework code, or to changes in git itself.
    
    Respectively:
    
     1. "mkdir -p" isn't needed because "git init" itself will create
        leading directories if needed.
    
     2. We don't need to check the number of arguments anymore, instead
        we'll feed "git init" with "$@". It will die if given too many
        arguments.
    
     3. We won't ever hit that "Cannot setup test environment"
        error.
    
        Checking the test environment sanity when doing "git init" dates
        back to eea420693be (t0000: catch trivial pilot errors.,
        2005-12-10) and 2ccd2027b01 (trivial: check, if t/trash directory
        was successfully created, 2006-01-05).
    
        We can also see it in another form a bit later in my own
        0d314ce834d (test-lib: use subshell instead of cd $new && .. && cd
        $old, 2010-08-30).
    
        But since 2006f0adaee (t/test-lib: make sure Git has already been
        built, 2012-09-17) we already check if we have a built git
        earlier.
    
        The one thing this was testing after that 2012 change was that
        we'd just built "git", but not "git-init", but since
        3af4c7156c4 (tests: respect GIT_TEST_INSTALLED when initializing
        repositories, 2018-11-12) we invoke "git", not "git-init".
    
        So all of that's been checked already, and we don't need to
        re-check it here.
    
     4. We don't need to move .git/hooks out of the way.
    
        That dates back to c09a69a83e3 (Disable hooks during tests.,
        2005-10-16), since then hooks became disabled by default in
        f98f8cbac01 (Ship sample hooks with .sample suffix, 2008-06-24).
    
        So the hooks were already disabled by default, but as can be seen
        from "mkdir .git/hooks" changes various tests needed to re-setup
        that directory. Now they no longer do.
    
     5. Since we don't need to move the .git/hooks directory we don't need
        the subshell here either.
    
    In the end it turns out that all we needed was a plain "git init"
    invocation with a custom --template directory.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/test-lib-functions.sh
+++ t/test-lib-functions.sh
@@ -1252,18 +1252,9 @@ test_atexit () {
 # Most tests can use the created repository, but some may need to create more.
 # Usage: test_create_repo <directory>
 test_create_repo () {
-	test "$#" = 1 ||
-	BUG "not 1 parameter to test-create-repo"
-	repo="$1"
-	mkdir -p "$repo"
-	(
-		cd "$repo" || error "Cannot setup test environment"
-		"${GIT_TEST_INSTALLED:-$GIT_EXEC_PATH}/git$X" \
-			init \
-			"--template=$GIT_BUILD_DIR/templates/blt/" >&3 2>&4 ||
-		error "cannot run git init -- have you built things yet?"
-		mv .git/hooks .git/hooks-disabled
-	) || exit
+	"${GIT_TEST_INSTALLED:-$GIT_EXEC_PATH}/git$X" \
+		init \
+		"--template=$GIT_BUILD_DIR/templates/blt/" "$@" >&3 2>&4
 }
 
 # This function helps on symlink challenged file systems when it is not
