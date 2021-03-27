f6d7c14c273f256af9f380eabd62fbd8cbc111e3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-03-27 19:03:07 +0100

    usage.c: add a non-fatal bug() function to go with BUG()
    Add a bug() function to use in cases where we'd like to indicate a
    runtime BUG(), but would like to deref the BUG() call because we're
    possibly accumulating more bug() callers to exhaustively indicate what
    went wrong. This will be used in place of optbug() in parse-options.c
    
    Any caller to bug() must follow up such calls with BUG_if_bug(), and
    as the tests and documentation here show we'll catch missing
    BUG_if_bug() invocations in our exit() wrapper.
    
    I'd previously proposed this as part of another series[1], in that
    use-case we ended thinking a BUG() would be better (and eventually
    96e41f58fe1 (fsck: report invalid object type-path combinations,
    2021-10-01) ended up with neither). Here I'll be converting existing
    in-tree users to this, so hopefully this won't be controversial.
    
    I'm not bothering to support the "else" branch of
    "HAVE_VARIADIC_MACROS". Since 765dc168882 (git-compat-util: always
    enable variadic macros, 2021-01-28) we've had a hard dependency on
    them, and manually undefining the macro will nowadays result in a hard
    compilation error. We should follow up with [2] instead and remove the
    "else" codepath.
    
    1. https://lore.kernel.org/git/YGRXomWwRYPdXZi3@coredump.intra.peff.net/
    2. https://lore.kernel.org/git/cover-0.2-00000000000-20210412T105422Z-avarab@gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/t0210-trace2-normal.sh
+++ t/t0210-trace2-normal.sh
@@ -168,6 +168,58 @@ test_expect_success 'BUG messages are written to trace2' '
 	test_cmp expect actual
 '
 
+test_expect_success 'bug messages with BUG_if_bug() are written to trace2' '
+	test_when_finished "rm trace.normal actual expect" &&
+	test_expect_code 99 env GIT_TRACE2="$(pwd)/trace.normal" \
+		test-tool trace2 008bug 2>err &&
+	cat >expect <<-\EOF &&
+	a bug message
+	another bug message
+	see bug() output above
+	EOF
+	sed "s/^.*: //" <err >actual &&
+	test_cmp expect actual &&
+
+	perl "$TEST_DIRECTORY/t0210/scrub_normal.perl" <trace.normal >actual &&
+	cat >expect <<-EOF &&
+		version $V
+		start _EXE_ trace2 008bug
+		cmd_name trace2 (trace2)
+		error a bug message
+		error another bug message
+		error see bug() output above
+		exit elapsed:_TIME_ code:99
+		atexit elapsed:_TIME_ code:99
+	EOF
+	test_cmp expect actual
+'
+
+test_expect_success 'bug messages without BUG_if_bug() are written to trace2' '
+	test_when_finished "rm trace.normal actual expect" &&
+	test_expect_code 99 env GIT_TRACE2="$(pwd)/trace.normal" \
+		test-tool trace2 009bug_BUG 2>err &&
+	cat >expect <<-\EOF &&
+	a bug message
+	another bug message
+	had bug() output above, in addition missed BUG_if_bug() call
+	EOF
+	sed "s/^.*: //" <err >actual &&
+	test_cmp expect actual &&
+
+	perl "$TEST_DIRECTORY/t0210/scrub_normal.perl" <trace.normal >actual &&
+	cat >expect <<-EOF &&
+		version $V
+		start _EXE_ trace2 009bug_BUG
+		cmd_name trace2 (trace2)
+		error a bug message
+		error another bug message
+		error had bug() output above, in addition missed BUG_if_bug() call
+		exit elapsed:_TIME_ code:99
+		atexit elapsed:_TIME_ code:99
+	EOF
+	test_cmp expect actual
+'
+
 sane_unset GIT_TRACE2_BRIEF
 
 # Now test without environment variables and get all Trace2 settings
