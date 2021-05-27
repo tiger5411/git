be1d7c7636baac498797205601fa91ab679d50c8
Emily Shaffer <emilyshaffer@google.com>
2021-05-26 17:08:38 -0700

    merge: use config-based hooks for post-merge hook
    Teach post-merge to use the hook.h library instead of the run-command.h
    library to run hooks. This means that post-merge hooks can come from the
    config as well as from the hookdir. post-merge is invoked only from
    builtin/merge.c.
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

--- builtin/merge.c
+++ builtin/merge.c
@@ -448,6 +448,7 @@ static void finish(struct commit *head_commit,
 		   const struct object_id *new_head, const char *msg)
 {
 	struct strbuf reflog_message = STRBUF_INIT;
+	struct run_hooks_opt opt;
 	const struct object_id *head = &head_commit->object.oid;
 
 	if (!msg)
@@ -489,7 +490,10 @@ static void finish(struct commit *head_commit,
 	}
 
 	/* Run a post-merge hook */
-	run_hook_le(NULL, "post-merge", squash ? "1" : "0", NULL);
+	run_hooks_opt_init_async(&opt);
+	strvec_push(&opt.args, squash ? "1" : "0");
+	run_hooks("post-merge", &opt);
+	run_hooks_opt_clear(&opt);
 
 	apply_autostash(git_path_merge_autostash(the_repository));
 	strbuf_release(&reflog_message);
