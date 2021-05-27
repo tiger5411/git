2f50cfe7b92f45f2f23b91997be3437628729f6e
Emily Shaffer <emilyshaffer@google.com>
2021-05-26 17:08:21 -0700

    hook: introduce git-hook subcommand
    Add a new subcommand, git-hook, which will be used to ease config-based
    hook management. This command will handle parsing configs to compose a
    list of hooks to run for a given event, as well as adding or modifying
    hook configs in an interactive fashion.
    
    Start with 'git hook list <hookname>', which checks the known configs in
    order to create an ordered list of hooks to run on a given hook event.
    
    Multiple commands can be specified for a given hook by providing
    multiple "hook.<hookname>.command = <path-to-hook>" lines. Hooks will be
    run in config order. If more properties need to be set on a given hook
    in the future, commands can also be specified by providing
    "hook.<hookname>.command = <hookcmd-name>", as well as a "[hookcmd
    <hookcmd-name>]" subsection; this subsection should contain a
    "hookcmd.<hookcmd-name>.command = <path-to-hook>" line.
    
    For example:
    
      $ git config --list | grep ^hook
      hook.pre-commit.command=baz
      hook.pre-commit.command=~/bar.sh
      hookcmd.baz.command=~/baz/from/hookcmd.sh
    
      $ git hook list pre-commit
      global: ~/baz/from/hookcmd.sh
      local: ~/bar.sh
    
    Signed-off-by: Emily Shaffer <emilyshaffer@google.com>
    Signed-off-by: Junio C Hamano <gitster@pobox.com>

---
+++ builtin/hook.c
@@ -0,0 +1,65 @@
+#include "cache.h"
+#include "builtin.h"
+#include "config.h"
+#include "hook.h"
+#include "parse-options.h"
+#include "strbuf.h"
+
+static const char * const builtin_hook_usage[] = {
+	N_("git hook list <hookname>"),
+	NULL
+};
+
+static int list(int argc, const char **argv, const char *prefix)
+{
+	struct list_head *head, *pos;
+	const char *hookname = NULL;
+
+	struct option list_options[] = {
+		OPT_END(),
+	};
+
+	argc = parse_options(argc, argv, prefix, list_options,
+			     builtin_hook_usage, 0);
+
+	if (argc < 1) {
+		usage_msg_opt(_("You must specify a hook event name to list."),
+			      builtin_hook_usage, list_options);
+	}
+
+	hookname = argv[0];
+
+	head = hook_list(hookname);
+
+	if (list_empty(head)) {
+		printf(_("no commands configured for hook '%s'\n"),
+		       hookname);
+		return 0;
+	}
+
+	list_for_each(pos, head) {
+		struct hook *item = list_entry(pos, struct hook, list);
+		if (item)
+			printf("%s: %s\n",
+			       config_scope_name(item->origin),
+			       item->command.buf);
+	}
+
+	clear_hook_list(head);
+
+	return 0;
+}
+
+int cmd_hook(int argc, const char **argv, const char *prefix)
+{
+	struct option builtin_hook_options[] = {
+		OPT_END(),
+	};
+	if (argc < 2)
+		usage_with_options(builtin_hook_usage, builtin_hook_options);
+
+	if (!strcmp(argv[1], "list"))
+		return list(argc - 1, argv + 1, prefix);
+
+	usage_with_options(builtin_hook_usage, builtin_hook_options);
+}
