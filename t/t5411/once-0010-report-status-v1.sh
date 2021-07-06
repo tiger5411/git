92bfd8a87b8b17338b471169a24d896343f281d0
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-06 13:05:56 +0200

    tests: replace [de]packetize() shell+perl test-tool pkt-line
    The shell+perl "[de]packetize()" helper functions were added in
    4414a150025 (t/lib-git-daemon: add network-protocol helpers,
    2018-01-24), and around the same time we added the "pkt-line" helper
    in 74e70029615 (test-pkt-line: introduce a packet-line test helper,
    2018-03-14).
    
    For some reason it seems we've mostly used the shell+perl version
    instead of the helper since then. There were discussions around
    88124ab2636 (test-lib-functions: make packetize() more efficient,
    2020-03-27) and cacae4329fa (test-lib-functions: simplify packetize()
    stdin code, 2020-03-29) to improve them and make them more efficient.
    
    Let's instead just use the test helper, I think this results in both
    more legible code, and for anyone who cares about efficiency it'll be
    faster.
    
    The conversion away from extract_haves() in
    t5410-receive-pack-alternates.sh and the "just pick out" logic in
    t5570-git-daemon.sh isn't strictly required for this migration, but in
    this case it's easy enough to test_cmp the whole output, so let's just
    do that.
    
    Similarly, there are cases here of changing "printf 0000" to a version
    that'll emit a trailing newline after "0000", or where we can do away
    with the "0000" trailer. Let's just always include, and include it in
    the same way.
    
    We can't convert all the users of packetize(), it has a feature the
    test-tool is missing. This'll be addressed in the subsequent commit.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/t5411/once-0010-report-status-v1.sh
+++ t/t5411/once-0010-report-status-v1.sh
@@ -33,15 +33,13 @@ test_expect_success "proc-receive: report status v1" '
 			printf "%s %s refs/heads/main\0report-status object-format=$GIT_DEFAULT_HASH\n" \
 				$A $B | packetize
 		fi &&
-		printf "%s %s refs/for/main/topic1\n" \
-			$ZERO_OID $A | packetize &&
-		printf "%s %s refs/heads/foo\n" \
-			$ZERO_OID $A | packetize &&
-		printf "%s %s refs/for/next/topic\n" \
-			$ZERO_OID $A | packetize &&
-		printf "%s %s refs/for/main/topic2\n" \
-			$ZERO_OID $A | packetize &&
-		printf 0000 &&
+		test-tool pkt-line pack <<-EOF &&
+		$ZERO_OID $A refs/for/main/topic1
+		$ZERO_OID $A refs/heads/foo
+		$ZERO_OID $A refs/for/next/topic
+		$ZERO_OID $A refs/for/main/topic2
+		0000
+		EOF
 		printf "" | git -C "$upstream" pack-objects --stdout
 	} | git receive-pack "$upstream" --stateless-rpc \
 	>out 2>&1 &&
