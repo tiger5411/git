abb41616e1f3959b6204c6e82c9154b1bed48b8b
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-08 12:06:54 +0200

    protocol v2 tests: split up and rename t5702-protocol-v2.sh
    The protocol v2 tests in t/t5702-protocol-v2.sh take around 12 seconds
    to run on my machine, which is annoying when trying to test something
    covered by a test at the very end.
    
    More importantly, some these tests would be needlessly skipped since
    we are testing both git:// and http:// protocols. I.e. if
    GIT_TEST_GIT_DAEMON is true (or we otherwise can't start the daemon)
    we'd skip them at the very start, including the later file:// tests,
    and http:// tests.
    
    Those http tests in turn would cause an early exit depend on
    GIT_TEST_HTTPD and our ability to start apache, even though we had
    tests declared after those that didn't rely on http:// or apache.
    
    With this change on my 8 core machine the overall time is reduced to
    3-4 seconds under -j8, "user" time is about the same, perhaps 100ms or
    so longer due to some duplicate setup work.
    
    This diff is scary, but when viewed under the --color-moved detection
    we can see that only a few lines of setup are changed, i.e. we don't
    need a "stop_git_daemon" anymore (it's implicit atexit via test_done
    in its own test), GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main was not
    needed for some of these, and I've removed comments whose only purpose
    was to separate tests in file I'm splitting up.
    
    I'm also renumbering t57* a bit for this move. Now t570* is most
    protocol v1 and v2 tests, i.e. git://, file://, http://
    etc. backends. t571* is tests of narrow functionality such as
    --negotiate-only or protocol violations, and t572* is protocol "verbs"
    like as shallow, packfile-uri, filters etc. I split up the
    "packfile-uri" tests into two files since they took a relatively long
    time, and I mean to extend the .gitmodules tests significantly in a
    follow-up series.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

---
+++ t/t5703-protocol-v2-file.sh
@@ -0,0 +1,232 @@
+#!/bin/sh
+
+test_description="Test protocol v2 with 'file://' transport"
+
+TEST_NO_CREATE_REPO=1
+
+GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
+export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+
+. ./test-lib.sh
+
+# Test protocol v2 with 'file://' transport
+#
+test_expect_success 'create repo to be served by file:// transport' '
+	git init file_parent &&
+	test_commit -C file_parent one
+'
+
+test_expect_success 'list refs with file:// using protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -c protocol.version=2 \
+		ls-remote --symref "file://$(pwd)/file_parent" >actual &&
+
+	# Server responded using protocol v2
+	grep "git< version 2" log &&
+
+	git ls-remote --symref "file://$(pwd)/file_parent" >expect &&
+	test_cmp expect actual
+'
+
+test_expect_success 'ls-remote handling a bad client using file:// protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	cat >expect <<-EOF &&
+	$(git -C file_parent rev-parse refs/heads/main)$(printf "\t")refs/heads/main
+	EOF
+	env \
+		GIT_TRACE_PACKET="$(pwd)/log" \
+		GIT_TEST_PROTOCOL_BAD_LS_REFS=true \
+		git -c protocol.version=2 \
+		ls-remote "file://$(pwd)/file_parent" main >actual 2>err &&
+
+	test_must_be_empty err &&
+	test_cmp expect actual &&
+	grep "git> test-bad-client$" log
+'
+
+test_expect_success 'ref advertisement is filtered with ls-remote using protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -c protocol.version=2 \
+		ls-remote "file://$(pwd)/file_parent" main >actual &&
+
+	cat >expect <<-EOF &&
+	$(git -C file_parent rev-parse refs/heads/main)$(printf "\t")refs/heads/main
+	EOF
+
+	test_cmp expect actual
+'
+
+test_expect_success 'server-options are sent when using ls-remote' '
+	test_when_finished "rm -f log" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -c protocol.version=2 \
+		ls-remote -o hello -o world "file://$(pwd)/file_parent" main >actual &&
+
+	cat >expect <<-EOF &&
+	$(git -C file_parent rev-parse refs/heads/main)$(printf "\t")refs/heads/main
+	EOF
+
+	test_cmp expect actual &&
+	grep "server-option=hello" log &&
+	grep "server-option=world" log
+'
+
+test_expect_success 'warn if using server-option with ls-remote with legacy protocol' '
+
+	cat >err.expect <<-\EOF &&
+	hint: see protocol.version in '"'"'git help config'"'"' for more details
+	fatal: server options require protocol version 2 or later
+	EOF
+	test_must_fail env GIT_TEST_PROTOCOL_VERSION=0 git -c protocol.version=0 \
+		ls-remote -o hello -o world "file://$(pwd)/file_parent" main >out 2>err.actual &&
+
+	test_must_be_empty out &&
+	grep -v "^fatal: the remote end hung up unexpectedly$" err.actual >err.filtered &&
+	test_cmp err.expect err.filtered
+'
+
+test_expect_success 'clone with file:// using protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -c protocol.version=2 \
+		clone "file://$(pwd)/file_parent" file_child &&
+
+	git -C file_child log -1 --format=%s >actual &&
+	git -C file_parent log -1 --format=%s >expect &&
+	test_cmp expect actual &&
+
+	# Server responded using protocol v2
+	grep "clone< version 2" log &&
+
+	# Client sent ref-prefixes to filter the ref-advertisement
+	grep "ref-prefix HEAD" log &&
+	grep "ref-prefix refs/heads/" log &&
+	grep "ref-prefix refs/tags/" log
+'
+
+test_expect_success 'clone of empty repo propagates name of default branch' '
+	test_when_finished "rm -rf file_empty_parent file_empty_child" &&
+
+	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME= \
+	git -c init.defaultBranch=mydefaultbranch init file_empty_parent &&
+
+	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME= \
+	git -c init.defaultBranch=main -c protocol.version=2 \
+		clone "file://$(pwd)/file_empty_parent" file_empty_child &&
+	grep "refs/heads/mydefaultbranch" file_empty_child/.git/HEAD
+'
+
+test_expect_success '...but not if explicitly forbidden by config' '
+	test_when_finished "rm -rf file_empty_parent file_empty_child" &&
+
+	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME= \
+	git -c init.defaultBranch=mydefaultbranch init file_empty_parent &&
+	test_config -C file_empty_parent lsrefs.unborn ignore &&
+
+	GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME= \
+	git -c init.defaultBranch=main -c protocol.version=2 \
+		clone "file://$(pwd)/file_empty_parent" file_empty_child &&
+	! grep "refs/heads/mydefaultbranch" file_empty_child/.git/HEAD
+'
+
+test_expect_success 'fetch with file:// using protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	test_commit -C file_parent two &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -C file_child -c protocol.version=2 \
+		fetch origin &&
+
+	git -C file_child log -1 --format=%s origin/main >actual &&
+	git -C file_parent log -1 --format=%s >expect &&
+	test_cmp expect actual &&
+
+	# Server responded using protocol v2
+	grep "fetch< version 2" log
+'
+
+test_expect_success 'ref advertisement is filtered during fetch using protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	test_commit -C file_parent three &&
+	git -C file_parent branch unwanted-branch three &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -C file_child -c protocol.version=2 \
+		fetch origin main &&
+
+	git -C file_child log -1 --format=%s origin/main >actual &&
+	git -C file_parent log -1 --format=%s >expect &&
+	test_cmp expect actual &&
+
+	grep "refs/heads/main" log &&
+	! grep "refs/heads/unwanted-branch" log
+'
+
+test_expect_success 'server-options are sent when fetching' '
+	test_when_finished "rm -f log" &&
+
+	test_commit -C file_parent four &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -C file_child -c protocol.version=2 \
+		fetch -o hello -o world origin main &&
+
+	git -C file_child log -1 --format=%s origin/main >actual &&
+	git -C file_parent log -1 --format=%s >expect &&
+	test_cmp expect actual &&
+
+	grep "server-option=hello" log &&
+	grep "server-option=world" log
+'
+
+test_expect_success 'fetch handling a bad client using file:// protocol v2' '
+	test_when_finished "rm -f log" &&
+
+	test_commit -C file_parent five &&
+
+	cat >err.expect <<-\EOF &&
+	fatal: unexpected line: '"'"'test-bad-client'"'"'
+	EOF
+	test_must_fail env \
+		GIT_TRACE_PACKET="$(pwd)/log" \
+		GIT_TEST_PROTOCOL_BAD_FETCH=true \
+		git -C file_child -c protocol.version=2 \
+		fetch >out 2>err.actual &&
+
+	test_must_be_empty out &&
+	grep -v "^fatal: the remote end hung up unexpectedly$" err.actual >err.filtered &&
+	test_cmp err.expect err.filtered &&
+
+	grep "fetch> test-bad-client$" log >sent-bad-request &&
+	test_file_not_empty sent-bad-request
+'
+
+test_expect_success 'server-options are sent when cloning' '
+	test_when_finished "rm -rf log myclone" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -c protocol.version=2 \
+		clone --server-option=hello --server-option=world \
+		"file://$(pwd)/file_parent" myclone &&
+
+	grep "server-option=hello" log &&
+	grep "server-option=world" log
+'
+
+test_expect_success 'upload-pack respects config using protocol v2' '
+	test_when_finished "rm -rf server client" &&
+	git init server &&
+	write_script server/.git/hook <<-\EOF &&
+		touch hookout
+		"$@"
+	EOF
+	test_commit -C server one &&
+
+	test_config_global uploadpack.packobjectshook ./hook &&
+	test_path_is_missing server/.git/hookout &&
+	git -c protocol.version=2 clone "file://$(pwd)/server" client &&
+	test_path_is_file server/.git/hookout
+'
+
+test_done
---
+++ t/t5722-protocol-v2-shallow-filter-deepen.sh
@@ -0,0 +1,217 @@
+#!/bin/sh
+
+test_description='Test shallow fetches, deepening and filters with protocol v2'
+
+GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME=main
+export GIT_TEST_DEFAULT_INITIAL_BRANCH_NAME
+
+. ./test-lib.sh
+
+test_expect_success 'setup filter tests' '
+	git init server &&
+
+	# 1 commit to create a file, and 1 commit to modify it
+	test_commit -C server message1 a.txt &&
+	test_commit -C server message2 a.txt &&
+	git -C server config protocol.version 2 &&
+	git -C server config uploadpack.allowfilter 1 &&
+	git -C server config uploadpack.allowanysha1inwant 1 &&
+	git -C server config protocol.version 2
+'
+
+test_expect_success 'partial clone' '
+	test_when_finished "rm -rf trace client" &&
+	GIT_TRACE_PACKET="$(pwd)/trace" git -c protocol.version=2 \
+		clone --filter=blob:none "file://$(pwd)/server" client &&
+	grep "version 2" trace &&
+
+	# Ensure that the old version of the file is missing
+	git -C client rev-list --quiet --objects --missing=print main \
+		>observed.oids &&
+	grep "$(git -C server rev-parse message1:a.txt)" observed.oids &&
+
+	# Ensure that client passes fsck
+	git -C client fsck
+'
+
+test_expect_success 'dynamically fetch missing object, does not list refs' '
+	test_when_finished "rm -rf trace client" &&
+	git -c protocol.version=2 \
+		clone --filter=blob:none "file://$(pwd)/server" client &&
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client -c protocol.version=2 \
+		cat-file -p $(git -C server rev-parse message1:a.txt) &&
+	grep "version 2" trace &&
+	! grep "git> command=ls-refs" trace
+'
+
+test_expect_success 'partial fetch' '
+	test_when_finished "rm -rf trace client" &&
+	git init client &&
+	SERVER="file://$(pwd)/server" &&
+
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client -c protocol.version=2 \
+		fetch --filter=blob:none "$SERVER" main:refs/heads/other &&
+	grep "version 2" trace &&
+
+	# Ensure that the old version of the file is missing
+	git -C client rev-list --quiet --objects --missing=print other \
+		>observed.oids &&
+	grep "$(git -C server rev-parse message1:a.txt)" observed.oids &&
+
+	# Ensure that client passes fsck
+	git -C client fsck
+'
+
+test_expect_success 'do not advertise filter if not configured to do so' '
+	test_when_finished "rm -rf trace" &&
+	SERVER="file://$(pwd)/server" &&
+
+	git -C server config uploadpack.allowfilter 1 &&
+	GIT_TRACE_PACKET="$(pwd)/trace" git -c protocol.version=2 \
+		ls-remote "$SERVER" &&
+	grep "fetch=.*filter" trace &&
+
+	rm "$(pwd)/trace" &&
+	git -C server config uploadpack.allowfilter 0 &&
+	GIT_TRACE_PACKET="$(pwd)/trace" git -c protocol.version=2 \
+		ls-remote "$SERVER" &&
+	grep "fetch=" trace >fetch_capabilities &&
+	! grep filter fetch_capabilities
+'
+
+test_expect_success 'partial clone warns if filter is not advertised' '
+	test_when_finished "rm -rf client" &&
+	git -C server config uploadpack.allowfilter 0 &&
+
+	cat >err.expect <<-\EOF &&
+	Cloning into '"'"'client'"'"'...
+	warning: filtering not recognized by server, ignoring
+	EOF
+	git -c protocol.version=2 \
+		clone --filter=blob:none "file://$(pwd)/server" client >out 2>err.actual &&
+
+	test_must_be_empty out &&
+	test_cmp err.expect err.actual
+'
+
+test_expect_success 'create repo to be served by file:// transport' '
+	git init file_parent &&
+	test_commit -C file_parent one &&
+	test_commit -C file_parent two &&
+	test_commit -C file_parent three
+'
+
+test_expect_success 'default refspec is used to filter ref when fetchcing' '
+	git -c protocol.version=2 clone "file://$(pwd)/file_parent" file_child &&
+
+	test_when_finished "rm -f log" &&
+
+	GIT_TRACE_PACKET="$(pwd)/log" git -C file_child -c protocol.version=2 \
+		fetch origin &&
+
+	git -C file_child log -1 --format=%s three >actual &&
+	git -C file_parent log -1 --format=%s three >expect &&
+	test_cmp expect actual &&
+
+	grep "ref-prefix refs/heads/" log &&
+	grep "ref-prefix refs/tags/" log
+'
+
+test_expect_success 'even with handcrafted request, filter does not work if not advertised' '
+	git -C server config uploadpack.allowfilter 0 &&
+
+	# Custom request that tries to filter even though it is not advertised.
+	test-tool pkt-line pack >in <<-EOF &&
+	command=fetch
+	object-format=$(test_oid algo)
+	0001
+	want $(git -C server rev-parse main)
+	filter blob:none
+	0000
+	EOF
+
+	cat >err.expect <<-\EOF &&
+	fatal: unexpected line: '"'"'filter blob:none'"'"'
+	EOF
+	test_must_fail test-tool -C server serve-v2 --stateless-rpc \
+		<in >out 2>err.actual &&
+
+	test_must_be_empty out &&
+	test_cmp err.expect err.actual &&
+
+	# Exercise to ensure that if advertised, filter works
+	git -C server config uploadpack.allowfilter 1 &&
+	test-tool -C server serve-v2 --stateless-rpc <in >out 2>err &&
+	test_must_be_empty err
+'
+
+test_expect_success 'upload-pack respects client shallows' '
+	test_when_finished "rm -rf server client trace" &&
+
+	git init server &&
+	test_commit -C server base &&
+	test_commit -C server client_has &&
+
+	git clone --depth=1 "file://$(pwd)/server" client &&
+
+	# Add extra commits to the client so that the whole fetch takes more
+	# than 1 request (due to negotiation)
+	test_commit_bulk -C client --id=c 32 &&
+
+	git -C server checkout -b newbranch base &&
+	test_commit -C server client_wants &&
+
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client -c protocol.version=2 \
+		fetch origin newbranch &&
+	# Ensure that protocol v2 is used
+	grep "fetch< version 2" trace
+'
+
+test_expect_success 'ensure that multiple fetches in same process from a shallow repo works' '
+	test_when_finished "rm -rf server client trace" &&
+
+	test_create_repo server &&
+	test_commit -C server one &&
+	test_commit -C server two &&
+	test_commit -C server three &&
+	git clone --shallow-exclude two "file://$(pwd)/server" client &&
+
+	git -C server tag -a -m "an annotated tag" twotag two &&
+
+	# Triggers tag following (thus, 2 fetches in one process)
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client -c protocol.version=2 \
+		fetch --shallow-exclude one origin &&
+	# Ensure that protocol v2 is used
+	grep "fetch< version 2" trace
+'
+
+test_expect_success 'deepen-relative' '
+	test_when_finished "rm -rf server client trace" &&
+
+	test_create_repo server &&
+	test_commit -C server one &&
+	test_commit -C server two &&
+	test_commit -C server three &&
+	git clone --depth 1 "file://$(pwd)/server" client &&
+	test_commit -C server four &&
+
+	# Sanity check that only "three" is downloaded
+	git -C client log --pretty=tformat:%s main >actual &&
+	echo three >expected &&
+	test_cmp expected actual &&
+
+	GIT_TRACE_PACKET="$(pwd)/trace" git -C client -c protocol.version=2 \
+		fetch --deepen=1 origin &&
+	# Ensure that protocol v2 is used
+	grep "fetch< version 2" trace &&
+
+	git -C client log --pretty=tformat:%s origin/main >actual &&
+	cat >expected <<-\EOF &&
+	four
+	three
+	two
+	EOF
+	test_cmp expected actual
+'
+
+test_done
