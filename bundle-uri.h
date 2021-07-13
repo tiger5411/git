5bf95862322c8c9b5649ba642328f84fbd99319c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-13 11:15:25 +0200

    bundle-uri client: add "bundle-uri" parsing + tests
    Add a "test-tool bundle-uri parse" which parses the format defined in
    the newly specified "bundle-uri" command.
    
    As note in the "bundle-uri" section in protocol-v2.txt we haven't
    specified any key-values yet, just URI lines, but we should parse
    their format for conformity with the spec.
    
    We need to make sure our future client doesn't die if this optional
    data is ever provided by the server, and that we've covered all the
    edge cases with these key-values in our specification. Let's add and
    test a bundle_uri_parse_line() to do that.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- bundle-uri.h
+++ bundle-uri.h
@@ -3,6 +3,7 @@
 #include "repository.h"
 #include "pkt-line.h"
 #include "strbuf.h"
+#include "string-list.h"
 
 /**
  * API used by serve.[ch].
@@ -10,4 +11,19 @@
 int bundle_uri_advertise(struct repository *r, struct strbuf *value);
 int bundle_uri_command(struct repository *r, struct packet_reader *request);
 
+/**
+ * General API for {transport,connect}.c etc.
+ */
+
+/**
+ * bundle_uri_parse_line() returns 0 when a valid bundle-uri has been
+ * added to `bundle_uri`, <0 on error.
+ */
+int bundle_uri_parse_line(struct string_list *bundle_uri, const char *line);
+
+/**
+ * Clear the `bundle_uri` list. Just a very thin wrapper on
+ * string_list_clear().
+ */
+void bundle_uri_string_list_clear(struct string_list *bundle_uri);
 #endif /* BUNDLE_URI_H */
