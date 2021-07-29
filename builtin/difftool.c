28b43789b115d17cf8cd67e4769b46895078c575
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-07-29 16:43:23 +0200

    parse-options API: remove OPTION_ARGUMENT feature
    As was noted in 1a85b49b87a (parse-options: make OPT_ARGUMENT() more
    useful, 2019-03-14) there's only ever been one user of the
    OPT_ARGUMENT(), that user was added in 20de316e334 (difftool: allow
    running outside Git worktrees with --no-index, 2019-03-14).
    
    The OPT_ARGUMENT() feature itself was added way back in
    580d5bffdea (parse-options: new option type to treat an option-like
    parameter as an argument., 2008-03-02), but as discussed in
    1a85b49b87a wasn't used until 20de316e334 in 2019.
    
    Now that the preceding commit has migrated this code over to using
    "struct strvec" to manage the "args" member of a "struct
    child_process", we can just use that directly instead of relying on
    OPT_ARGUMENT.
    
    This has a minor change in behavior in that if we'll pass --no-index
    we'll now always pass it as the first argument, before we'd pass it in
    whatever position the caller did. Preserving this was the real value
    of OPT_ARGUMENT(), but as it turns out we didn't need that either. We
    can always inject it as the first argument, the other end will parse
    it just the same.
    
    Note that we cannot remove the "out" and "cpidx" members of "struct
    parse_opt_ctx_t" added in 580d5bffdea, while they were introduced with
    OPT_ARGUMENT() we since used them for other things.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/difftool.c
+++ builtin/difftool.c
@@ -714,7 +714,7 @@ int cmd_difftool(int argc, const char **argv, const char *prefix)
 			    "tool returns a non - zero exit code")),
 		OPT_STRING('x', "extcmd", &extcmd, N_("command"),
 			   N_("specify a custom command for viewing diffs")),
-		OPT_ARGUMENT("no-index", &no_index, N_("passed to `diff`")),
+		OPT_BOOL(0, "no-index", &no_index, N_("passed to `diff`")),
 		OPT_END()
 	};
 	struct strvec args = STRVEC_INIT;
@@ -768,6 +768,8 @@ int cmd_difftool(int argc, const char **argv, const char *prefix)
 	 * each file that changed.
 	 */
 	strvec_push(&args, "diff");
+	if (no_index)
+		strvec_push(&args, "--no-index");
 	if (dir_diff)
 		strvec_pushl(&args, "--raw", "--no-abbrev", "-z", NULL);
 	strvec_pushv(&args, argv);
