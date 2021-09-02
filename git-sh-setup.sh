8eb1dfbff5daad12f7181dbbc5a5e71435c50744
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-02 17:16:16 +0200

    git-sh-setup: remove unused git_pager() function
    Remove the git_pager() function last referenced by non-test code in
    49eb8d39c78 (Remove contrib/examples/*, 2018-03-25).
    
    We can also remove the test for this added in 995bc22d7f8 (pager: move
    pager-specific setup into the build, 2016-08-04), the test that
    actually matters is the one added in e54c1f2d253 (pager: set LV=-c
    alongside LESS=FRSX, 2014-01-06) just above the removed test.
    
    I.e. we don't care if the "LESS" and "LV" variables are set by
    git-sh-setup anymore, no built-in uses them, we do care that pager.c
    sets them, which we still test for.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- git-sh-setup.sh
+++ git-sh-setup.sh
@@ -157,22 +157,6 @@ git_editor() {
 	eval "$GIT_EDITOR" '"$@"'
 }
 
-git_pager() {
-	if test -t 1
-	then
-		GIT_PAGER=$(git var GIT_PAGER)
-	else
-		GIT_PAGER=cat
-	fi
-	for vardef in @@PAGER_ENV@@
-	do
-		var=${vardef%%=*}
-		eval ": \"\${$vardef}\" && export $var"
-	done
-
-	eval "$GIT_PAGER" '"$@"'
-}
-
 sane_grep () {
 	GREP_OPTIONS= LC_ALL=C grep @@SANE_TEXT_GREP@@ "$@"
 }
