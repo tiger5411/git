01fe8987a8cd85c69345a32c464a3aeebfdf749e
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-06 01:02:32 +0200

    *.h _INIT macros: don't specify fields equal to 0
    Change the initialization of "struct strbuf" changed in
    cbc0f81d96f (strbuf: use designated initializers in STRBUF_INIT,
    2017-07-10) to omit specifying "alloc" and "len", as we do with other
    "alloc" and "len" (or "nr") in similar structs.
    
    Let's likewise omit the explicit initialization of all fields in the
    "struct ipc_client_connect_option" struct added in
    59c7b88198a (simple-ipc: add win32 implementation, 2021-03-15).
    
    Do the same for a few other initializers, e.g. STRVEC_INIT and
    CACHE_DEF_INIT.
    
    Finally, start incrementally changing the same pattern in
    "t/helper/test-run-command.c". This change was part of an earlier
    on-list version[1] of c90be786da9 (test-tool run-command: fix
    flip-flop init pattern, 2021-09-11).
    
    1. https://lore.kernel.org/git/patch-1.1-0aa4523ab6e-20210909T130849Z-avarab@gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- cache.h
+++ cache.h
@@ -1625,7 +1625,7 @@ struct cache_def {
 	int track_flags;
 	int prefix_len_stat_func;
 };
-#define CACHE_DEF_INIT { STRBUF_INIT, 0, 0, 0 }
+#define CACHE_DEF_INIT { STRBUF_INIT }
 static inline void cache_def_clear(struct cache_def *cache)
 {
 	strbuf_release(&cache->path);
