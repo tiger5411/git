c0b1ec90d4369889bcd75741e44883d0c0919fc6
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-07 20:37:56 +0200

    pack-write: refactor renaming in finish_tmp_packfile()
    Refactor the renaming in finish_tmp_packfile() into a helper
    function. The callers are now expected to pass a "name_buffer" ending
    in "pack-OID." instead of the previous "pack-", we then append "pack",
    "idx" or "rev" to it.
    
    By doing the strbuf_setlen() in rename_tmp_packfile() we re-use the
    buffer and avoid the repeated allocations we'd get if that function
    had its own temporary "struct strbuf".
    
    This approach of re-using the buffer does make the last user in
    pack-object.c's write_pack_file() slightly awkward, we needlessly do a
    strbuf_setlen() there just before the strbuf_release() for
    consistency. In subsequent changes we'll move that bitmap writing code
    around, so let's not skip the strbuf_setlen() now.
    
    The previous strbuf_reset() idiom originated with
    5889271114a (finish_tmp_packfile():use strbuf for pathname
    construction, 2014-03-03), which in turn was a minimal adjustment of
    pre-strbuf code added in 0e990530ae (finish_tmp_packfile(): a helper
    function, 2011-10-28).
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- pack-write.c
+++ pack-write.c
@@ -458,6 +458,18 @@ struct hashfile *create_tmp_packfile(char **pack_tmp_name)
 	return hashfd(fd, *pack_tmp_name);
 }
 
+static void rename_tmp_packfile(struct strbuf *nb, const char *source,
+				const char *ext)
+{
+	size_t nb_len = nb->len;
+
+	strbuf_addstr(nb, ext);
+	if (rename(source, nb->buf))
+		die_errno("unable to rename temporary '*.%s' file to '%s'",
+			  ext, nb->buf);
+	strbuf_setlen(nb, nb_len);
+}
+
 void finish_tmp_packfile(struct strbuf *name_buffer,
 			 const char *pack_tmp_name,
 			 struct pack_idx_entry **written_list,
@@ -466,7 +478,6 @@ void finish_tmp_packfile(struct strbuf *name_buffer,
 			 unsigned char hash[])
 {
 	const char *idx_tmp_name, *rev_tmp_name = NULL;
-	int basename_len = name_buffer->len;
 
 	if (adjust_shared_perm(pack_tmp_name))
 		die_errno("unable to make temporary pack file readable");
@@ -479,26 +490,10 @@ void finish_tmp_packfile(struct strbuf *name_buffer,
 	rev_tmp_name = write_rev_file(NULL, written_list, nr_written, hash,
 				      pack_idx_opts->flags);
 
-	strbuf_addf(name_buffer, "%s.pack", hash_to_hex(hash));
-
-	if (rename(pack_tmp_name, name_buffer->buf))
-		die_errno("unable to rename temporary pack file");
-
-	strbuf_setlen(name_buffer, basename_len);
-
-	strbuf_addf(name_buffer, "%s.idx", hash_to_hex(hash));
-	if (rename(idx_tmp_name, name_buffer->buf))
-		die_errno("unable to rename temporary index file");
-
-	strbuf_setlen(name_buffer, basename_len);
-
-	if (rev_tmp_name) {
-		strbuf_addf(name_buffer, "%s.rev", hash_to_hex(hash));
-		if (rename(rev_tmp_name, name_buffer->buf))
-			die_errno("unable to rename temporary reverse-index file");
-	}
-
-	strbuf_setlen(name_buffer, basename_len);
+	rename_tmp_packfile(name_buffer, pack_tmp_name, "pack");
+	rename_tmp_packfile(name_buffer, idx_tmp_name, "idx");
+	if (rev_tmp_name)
+		rename_tmp_packfile(name_buffer, rev_tmp_name, "rev");
 
 	free((void *)idx_tmp_name);
 }
