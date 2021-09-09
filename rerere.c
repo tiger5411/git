1db8564ee24b04bd504f34720ff455b74610cb0d
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-09 03:48:03 +0200

    rerere: fix segfault?
    See https://lore.kernel.org/git/87tuiufr3c.fsf@evledraar.gmail.com/

--- rerere.c
+++ rerere.c
@@ -157,6 +157,9 @@ static int has_rerere_resolution(const struct rerere_id *id)
 	const int both = RR_HAS_POSTIMAGE|RR_HAS_PREIMAGE;
 	int variant = id->variant;
 
+	if (variant == id->collection->status_nr)
+		return 1;
+
 	if (variant < 0)
 		return 0;
 	return ((id->collection->status[variant] & both) == both);
