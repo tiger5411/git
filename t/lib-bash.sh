2c6c9ea6c7b9f0c34d66c3fe6d4d1d59d1474e3a
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-16 13:31:57 +0200

    t/lib-bash.sh: stop insisting on !POSIXLY_CORRECT
    If the assertion that POSIXLY_CORRECT can't be set to run the
    completion (and now, prompt) tests was ever true, it doesn't seem to
    be true anymore. Let's drop this condition added in
    5c293a6be48 (tests: add initial bash completion tests, 2012-04-12).
    
    Let's add a message about what mode we're running under in case this
    ever cause problems. Now we'll emit a startup message similar to
    lib-gettext.sh (see 5e9637c6297 (i18n: add infrastructure for
    translating Git with gettext, 2011-11-18)):
    
        $ bash --posix t9902-completion.sh
        # lib-bash.sh: running under 5.1.8(1)-release in POSIXLY_CORRECT mode
        ok 1 - setup for __git_find_repo_path/__gitdir tests
        [...]
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- t/lib-bash.sh
+++ t/lib-bash.sh
@@ -2,17 +2,24 @@
 # to run under Bash; primarily intended for tests of the completion
 # script.
 
-if test -n "$BASH" && test -z "$POSIXLY_CORRECT"
+if test -n "$BASH"
 then
 	# we are in full-on bash mode
 	true
 elif type bash >/dev/null 2>&1
 then
-	# execute in full-on bash mode
-	unset POSIXLY_CORRECT
+	# execute with bash
 	exec bash "$0" "$@"
 else
 	skip_all="bash not available"
 fi
 
 . ./test-lib.sh
+
+posix_blurb=
+if test -n "$POSIXLY_CORRECT"
+then
+	posix_blurb=" in POSIXLY_CORRECT mode"
+fi
+
+say "# lib-bash.sh: running under $BASH_VERSION$posix_blurb"
