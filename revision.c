424fac7ab955b20c63f32cc9d1b97a3877546ef3
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-19 12:10:47 +0200

    revisions API: have release_revisions() release "cmdline"
    Extend the the release_revisions() function so that it frees the
    "cmdline" in the "struct ref_info". This in combination with a
    preceding change to free "commits" and "mailmap" means that we can
    whitelist another test under "TEST_PASSES_SANITIZE_LEAK=true".
    
    There was a proposal in [1] to do away with xstrdup()-ing this
    add_rev_cmdline(), perhaps that would be worthwhile, but for now let's
    just free() it.
    
    We could also make that a "char *" in "struct rev_cmdline_entry"
    itself, but since we own it let's expose it as a constant to outside
    callers. I proposed that in [2] but have since changed my mind. See
    14d30cdfc04 (ref-filter: fix memory leak in `free_array_item()`,
    2019-07-10), c514c62a4fd (checkout: fix leak of non-existent branch
    names, 2020-08-14) and other log history hits for "free((char *)" for
    prior art.
    
    This includes the tests we had false-positive passes on before my
    6798b08e848 (perl Git.pm: don't ignore signalled failure in
    _cmd_close(), 2022-02-01), now they pass for real.
    
    Since there are 66 tests matching t/t[0-9]*git-svn*.sh it's easier to
    list those that don't pass than to touch most of those 66. So let's
    introduce a "TEST_FAILS_SANITIZE_LEAK=true", which if set in the tests
    won't cause lib-git-svn.sh to set "TEST_PASSES_SANITIZE_LEAK=true.
    
    This change also marks all the tests that we removed
    "TEST_FAILS_SANITIZE_LEAK=true" from in an earlier commit due to
    removing the UNLEAK() from cmd_format_patch(), we can now assert that
    its API use doesn't leak any "struct rev_info" memory.
    
    This change also made commit "t5503-tagfollow.sh" pass on current
    master, but that would regress when combined with
    ps/fetch-atomic-fixup's de004e848a9 (t5503: simplify setup of test
    which exercises failure of backfill, 2022-03-03) (through no fault of
    that topic, that change started using "git clone" in the test, which
    has an outstanding leak). Let's leave that test out for now to avoid
    in-flight semantic conflicts.
    
    1. https://lore.kernel.org/git/YUj%2FgFRh6pwrZalY@carlos-mbp.lan/
    2. https://lore.kernel.org/git/87o88obkb1.fsf@evledraar.gmail.com/
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>
    
    fixup with bc0e0439040?

--- revision.c
+++ revision.c
@@ -2933,6 +2933,17 @@ static void release_revisions_commit_list(struct rev_info *revs)
 	revs->commits = NULL;
 }
 
+static void release_revisions_cmdline(struct rev_cmdline_info *cmdline)
+{
+	unsigned int i;
+
+	if (!cmdline)
+		return;
+	for (i = 0; i < cmdline->nr; i++)
+		free((char *)cmdline->rev[i].name);
+	FREE_AND_NULL(cmdline->rev);
+}
+
 static void release_revisions_mailmap(struct string_list *mailmap)
 {
 	if (!mailmap)
@@ -2947,6 +2958,7 @@ void release_revisions(struct rev_info *revs)
 		return;
 	release_revisions_commit_list(revs);
 	object_array_clear(&revs->pending);
+	release_revisions_cmdline(&revs->cmdline);
 	release_revisions_mailmap(revs->mailmap);
 }
 
