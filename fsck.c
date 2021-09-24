a188b05e7d83fbd22ad02d16de8312132ca44999
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-24 15:55:17 +0200

    fsck.[ch]: refactor out FSCK_OPTIONS_MISSING_GITMODULES
    As discussed in the preceding commit we'll only need this in
    "index-pack", so let's move it over there.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- fsck.c
+++ fsck.c
@@ -1335,21 +1335,3 @@ int git_fsck_config(const char *var, const char *value, void *cb)
 
 	return git_default_config(var, value, cb);
 }
-
-/*
- * Custom error callbacks that are used in more than one place.
- */
-
-int fsck_error_cb_print_missing_gitmodules(struct fsck_options *o,
-					   const struct object_id *oid,
-					   enum object_type object_type,
-					   enum fsck_msg_type msg_type,
-					   enum fsck_msg_id msg_id,
-					   const char *message)
-{
-	if (msg_id == FSCK_MSG_GITMODULES_MISSING) {
-		puts(oid_to_hex(oid));
-		return 0;
-	}
-	return fsck_error_function(o, oid, object_type, msg_type, msg_id, message);
-}
