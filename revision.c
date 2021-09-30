a93251edf85c50cda2ecb12fc0511770f8128b85
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-30 17:52:44 +0200

    revisions API: have release_revisions() release "mailmap"
    Extend the the release_revisions() function so that it frees the
    "prune_data" in the "struct ref_info".
    
    The log family of functions now calls the clear_mailmap() function
    added in fa8afd18e5a (revisions API: provide and use a
    release_revisions(), 2021-09-19), allowing us to whitelist some tests
    with "TEST_PASSES_SANITIZE_LEAK=true".
    
    Unfortunately having a pointer to a mailmap in "struct rev_info"
    instead of an embedded member that we "own" get a bit messy, as can be
    seen in the change to builtin/commit.c.
    
    When we free() this data we won't be able to tell apart a pointer to a
    "mailmap" on the heap from one on the stack. As seen in
    ea57bc0d41b (log: add --use-mailmap option, 2013-01-05) the "log"
    family allocates it on the heap, but in the find_author_by_nickname()
    code added in ea16794e430 (commit: search author pattern against
    mailmap, 2013-08-23) we allocated it on the stack instead.
    
    Ideally we'd simply change that member to a "struct string_list
    mailmap" and never free() the "mailmap" itself, but that would be a
    much larger change to the revisions API.
    
    We have code that needs to hand an existing "mailmap" to a "struct
    rev_info", while we could change all of that, let's not go there
    now.
    
    The complexity isn't in the ownership of the "mailmap" per-se, but
    that various things assume a "rev_info.mailmap == NULL" means "doesn't
    want mailmap", if we changed that to an init'd "struct string_list
    we'd need to carefully refactor things to change those assumptions.
    
    Let's instead always free() it, and simply declare that if you add
    such a "mailmap" it must be allocated on the heap. Any modern libc
    will correctly panic if we free() a stack variable, so this should be
    safe going forward.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- revision.c
+++ revision.c
@@ -2933,12 +2933,21 @@ static void release_revisions_commit_list(struct rev_info *revs)
 	revs->commits = NULL;
 }
 
+static void release_revisions_mailmap(struct string_list *mailmap)
+{
+	if (!mailmap)
+		return;
+	clear_mailmap(mailmap);
+	free(mailmap);
+}
+
 void release_revisions(struct rev_info *revs)
 {
 	if (!revs)
 		return;
 	release_revisions_commit_list(revs);
 	object_array_clear(&revs->pending);
+	release_revisions_mailmap(revs->mailmap);
 }
 
 static void add_child(struct rev_info *revs, struct commit *parent, struct commit *child)
