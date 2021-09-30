e6cabb3732cd5cc84ad7f981dfe29f4bb0bb47dd
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-09-30 16:41:22 +0200

    builtin/log.c: use release_revisions API
    In preparation for having the "log" family of functions make wider use
    of release_revisions() let's have them call it just before
    exiting. The release API still does nothing except
    "object_array_clear(&revs->pending)", but that will change in
    subsequent commits.
    
    This changes the "log", "whatchanged", "show", etc. commands, all of
    which live in this file.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/log.c
+++ builtin/log.c
@@ -294,6 +294,12 @@ static void cmd_log_init(int argc, const char **argv, const char *prefix,
 	cmd_log_init_finish(argc, argv, prefix, rev, opt);
 }
 
+static int cmd_log_deinit(int ret, struct rev_info *rev, struct setup_revision_opt *opt)
+{
+	release_revisions(rev);
+	return ret;
+}
+
 /*
  * This gives a rough estimate for how many commits we
  * will print out in the list.
@@ -557,7 +563,7 @@ int cmd_whatchanged(int argc, const char **argv, const char *prefix)
 	cmd_log_init(argc, argv, prefix, &rev, &opt);
 	if (!rev.diffopt.output_format)
 		rev.diffopt.output_format = DIFF_FORMAT_RAW;
-	return cmd_log_walk(&rev);
+	return cmd_log_deinit(cmd_log_walk(&rev), &rev, &opt);
 }
 
 static void show_tagger(const char *buf, struct rev_info *rev)
@@ -676,7 +682,7 @@ int cmd_show(int argc, const char **argv, const char *prefix)
 	cmd_log_init(argc, argv, prefix, &rev, &opt);
 
 	if (!rev.no_walk)
-		return cmd_log_walk(&rev);
+		return cmd_log_deinit(cmd_log_walk(&rev), &rev, &opt);
 
 	count = rev.pending.nr;
 	objects = rev.pending.objects;
@@ -731,8 +737,7 @@ int cmd_show(int argc, const char **argv, const char *prefix)
 			ret = error(_("unknown type: %d"), o->type);
 		}
 	}
-	free(objects);
-	return ret;
+	return cmd_log_deinit(ret, &rev, &opt);
 }
 
 /*
@@ -760,7 +765,7 @@ int cmd_log_reflog(int argc, const char **argv, const char *prefix)
 	rev.always_show_header = 1;
 	cmd_log_init_finish(argc, argv, prefix, &rev, &opt);
 
-	return cmd_log_walk(&rev);
+	return cmd_log_deinit(cmd_log_walk(&rev), &rev, &opt);
 }
 
 static void log_setup_revisions_tweak(struct rev_info *rev,
@@ -791,7 +796,7 @@ int cmd_log(int argc, const char **argv, const char *prefix)
 	opt.revarg_opt = REVARG_COMMITTISH;
 	opt.tweak = log_setup_revisions_tweak;
 	cmd_log_init(argc, argv, prefix, &rev, &opt);
-	return cmd_log_walk(&rev);
+	return cmd_log_deinit(cmd_log_walk(&rev), &rev, &opt);
 }
 
 /* format-patch */
