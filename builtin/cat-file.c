9dbb45c38f1eec19784f5db882ffd4475547ef0c
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-10-09 11:00:59 +0200

    cat-file: de-duplicate usage_with_options() invocations
    Switch to a "goto usage" pattern in cmd_cat_file(). This introduces no
    functional changes, but makes the code easier to amend in subsequent
    commits.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- builtin/cat-file.c
+++ builtin/cat-file.c
@@ -676,35 +676,35 @@ int cmd_cat_file(int argc, const char **argv, const char *prefix)
 		else if (argc == 1)
 			obj_name = argv[0];
 		else
-			usage_with_options(cat_file_usage, options);
+			goto usage;
 	}
 	if (!opt && !batch.enabled) {
 		if (argc == 2) {
 			exp_type = argv[0];
 			obj_name = argv[1];
 		} else
-			usage_with_options(cat_file_usage, options);
+			goto usage;
 	}
 	if (batch.enabled) {
 		if (batch.cmdmode != opt || argc)
-			usage_with_options(cat_file_usage, options);
+			goto usage;
 		if (batch.cmdmode && batch.all_objects)
 			die("--batch-all-objects cannot be combined with "
 			    "--textconv nor with --filters");
 	}
 
 	if ((batch.follow_symlinks || batch.all_objects) && !batch.enabled) {
-		usage_with_options(cat_file_usage, options);
+		goto usage;
 	}
 
 	if (force_path && opt != 'c' && opt != 'w') {
 		error("--path=<path> needs --textconv or --filters");
-		usage_with_options(cat_file_usage, options);
+		goto usage;
 	}
 
 	if (force_path && batch.enabled) {
 		error("--path=<path> incompatible with --batch");
-		usage_with_options(cat_file_usage, options);
+		goto usage;
 	}
 
 	if (batch.buffer_output < 0)
@@ -716,4 +716,6 @@ int cmd_cat_file(int argc, const char **argv, const char *prefix)
 	if (unknown_type && opt != 't' && opt != 's')
 		die("git cat-file --allow-unknown-type: use with -s or -t");
 	return cat_one_file(opt, exp_type, obj_name, unknown_type);
+usage:
+	usage_with_options(cat_file_usage, options);
 }
