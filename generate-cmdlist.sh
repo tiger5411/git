9ade98dd35729011f1baefa8be2cda942d72d50f
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-10-22 13:36:42 +0200

    experiment with eval

--- generate-cmdlist.sh
+++ generate-cmdlist.sh
@@ -5,18 +5,6 @@ die () {
 	exit 1
 }
 
-command_list () {
-	eval "grep -ve '^#' $exclude_programs" <"$1"
-}
-
-category_list () {
-	command_list "$1" |
-	cut -c 40- |
-	tr ' ' '\012' |
-	grep -v '^$' |
-	LC_ALL=C sort -u
-}
-
 get_synopsis () {
 	sed -n '
 		/^NAME/,/'"$1"'/H
@@ -28,43 +16,51 @@ get_synopsis () {
 }
 
 define_categories () {
-	echo
-	echo "/* Command categories */"
-	bit=0
-	category_list "$1" |
-	while read cat
-	do
-		echo "#define CAT_$cat (1UL << $bit)"
-		bit=$(($bit+1))
-	done
-	test "$bit" -gt 32 && die "Urgh.. too many categories?"
-}
+	format=$1
+	shift
 
-define_category_names () {
-	echo
-	echo "/* Category names */"
-	echo "static const char *category_names[] = {"
+	seen=":"
 	bit=0
-	category_list "$1" |
-	while read cat
+	while read cmd cats
 	do
-		echo "	\"$cat\", /* (1UL << $bit) */"
-		bit=$(($bit+1))
-	done
-	echo "	NULL"
-	echo "};"
+		bol=$(printf "%c" "$cmd")
+		if test "$bol" = "#"
+		then
+			continue
+		fi
+		
+		for cat in $(printf " %s " $cats)
+		do
+			case "$seen" in
+			*":$cat:"*)
+				continue;
+				;;
+			*)
+				printf "$format" "$cat" "$bit"
+				bit=$(($bit+1))
+				seen="$seen:$cat:"
+				;;
+			esac
+		done
+
+	done <"$1"
+	test "$bit" -gt 32 && die "Urgh.. too many categories?"
 }
 
 print_command_list () {
 	echo "static struct cmdname_help command_list[] = {"
 
-	command_list "$1" |
 	while read cmd rest
 	do
+		bol=$(printf "%c" "$cmd")
+		if test "$bol" = "#"
+		then
+			continue
+		fi
 		printf "	{ \"$cmd\", $(get_synopsis $cmd), 0"
 		printf " | CAT_%s" $rest
 		echo " },"
-	done
+	done <"$1"
 	echo "};"
 }
 
@@ -82,9 +78,17 @@ struct cmdname_help {
 	const char *help;
 	uint32_t category;
 };
+
+
+/* Command categories */
 "
-define_categories "$1"
+define_categories "#define CAT_%s (1UL << %s)\n" "$1"
 echo
-define_category_names "$1"
+echo "/* Category names */"
+echo "static const char *category_names[] = {"
+define_categories "\t\"%s\", /* (1UL << %s) */\n" "$1"
+echo "	NULL"
+echo "};"
+
 echo
 print_command_list "$1"
