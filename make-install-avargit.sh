6c6bf4e5135926598a61c9ebf551dab9a097807d
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2021-11-14 20:04:10 +0100

    series + mia: bump with leak fix

--- make-install-avargit.sh
+++ make-install-avargit.sh
@@ -205,7 +205,7 @@ test_compile () {
 		# avar/do-not-die-on-setup-gently-2 adds an "git
 		# ls-remote" to the test, so it's a "new" but old
 		# leak.
-		if ! GIT_SKIP_TESTS=t0002 GIT_TEST_HTTPD=1 GIT_TEST_PASSING_SANITIZE_LEAK=true GIT_TEST_PIPEFAIL=true make GIT_PROVE_OPTS="$GIT_PROVE_OPTS --exec /home/avar/g/bash/bash"
+		if ! GIT_SKIP_TESTS="t0002 t4131" GIT_TEST_HTTPD=1 GIT_TEST_PASSING_SANITIZE_LEAK=true GIT_TEST_PIPEFAIL=true make GIT_PROVE_OPTS="$GIT_PROVE_OPTS --exec /home/avar/g/bash/bash"
 		then
 			suggest_bisect "$(git rev-parse HEAD)"
 		fi
