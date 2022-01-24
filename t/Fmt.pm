d07ba810a44f02dcbd41765ac3a5862b34c91883
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-01-24 13:46:12 +0100

    Merge pull my finger request

--- t/Fmt.pm
+++ t/Fmt.pm
@@ -5,9 +5,6 @@ use warnings;
 use base 'TAP::Formatter::Console::ParallelSession';
 
 our %STATE;
-## TODO: This state machine needs to go past the "ok" line and grab
-## the comments emitted by e.g. "say_color_tap_comment_lines" in
-## test_ok_()
 sub result {
 	my $self = shift;
 	my $result = shift;
@@ -16,15 +13,15 @@ sub result {
 	my $test_name = $self->name;
 
 	# An AoO of test numbers and their output lines
-	$STATE{$test_name} ||= [[]];
+	$STATE{$test_name} ||= [{lines => []}];
 
-	push @{$STATE{$test_name}->[-1]} => $result;
+	push @{$STATE{$test_name}->[-1]->{lines}} => $result->raw;
 
 	# When we see a new test add a new AoA for its output. We do
 	# end up with the "plan" type as part of the last test, and
 	# might want to split it up
 	if ($result->type eq 'test') {
-		push @{$STATE{$test_name}} => [];
+		push @{$STATE{$test_name}} => {};
 	}
 
 	return $res;
@@ -33,6 +30,7 @@ sub result {
 package Fmt;
 use strict;
 use warnings;
+use List::MoreUtils qw(firstidx);
 use base 'TAP::Formatter::Console';
 
 sub open_test {
@@ -47,6 +45,31 @@ sub open_test {
 sub summary {
 	my $self = shift;
 	$self->SUPER::summary(@_);
+
+	## This state machine needs to go past the "ok" line and grab
+	## the comments emitted by e.g. "say_color_tap_comment_lines" in
+	## test_ok_()
+	for my $test (sort keys %STATE) {
+		for (my $i = 1; $i <= $#{$STATE{$test}}; $i++) {
+			my @lines = @{$STATE{$test}->[$i]->{lines}};
+			use Data::Dumper;
+			warn Dumper \@lines;
+			my $break = firstidx { $_ eq '' } @lines;
+			my @source = splice @lines, 0, $break;
+			splice @lines, 0, 1; # Splice out the '' item
+			warn Dumper \@source;
+			push @{$STATE{$test}->[$i - 1]->{lines}} => ('', @source);
+			use Data::Dumper;
+
+			$STATE{$test}->[$i]->{lines} = \@lines;
+
+			# Since we parsed out the source already,
+			# let's make it easily machine-readable, and
+			# parse the rest.
+			$STATE{$test}->[$i]->{source} = \@source;
+			$STATE{$test}->[$i]->{trace} =  [ grep /^\+ /, @lines ];
+		}
+	}
 	use Data::Dumper;
 	die Dumper [\%STATE, \@_];
 }
