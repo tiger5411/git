0508777c68e1d3245a46d7ed6bb2605edb9c75be
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-02-04 01:44:27 +0100

    CI: set PYTHON_PATH setting for osx-{clang,gcc} into "$jobname" case
    Move the setting of "PYTHON_PATH=[...]" to the "$jobname" case
    statement. This partially backs out of my 707d2f2fe86 (CI: use
    "$runs_on_pool", not "$jobname" to select packages & config,
    2021-11-23), now that we have a "osx-{clang,gcc}" anyway for setting
    "$CC" we might as well do away with this part of the "$runs_on_pool"
    case.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- ci/lib.sh
+++ ci/lib.sh
@@ -105,14 +105,6 @@ ubuntu-latest)
 
 	setenv --test GIT_TEST_HTTPD true
 	;;
-macos-latest)
-	if test "$jobname" = osx-gcc
-	then
-		MAKEFLAGS="$MAKEFLAGS PYTHON_PATH=$(which python3)"
-	else
-		MAKEFLAGS="$MAKEFLAGS PYTHON_PATH=$(which python2)"
-	fi
-	;;
 esac
 
 case "$jobname" in
@@ -170,10 +162,12 @@ linux-TEST-vars)
 	setenv --test GIT_TEST_CHECKOUT_WORKERS 2
 	;;
 osx-gcc)
+	MAKEFLAGS="$MAKEFLAGS PYTHON_PATH=$(which python3)"
 	CC=gcc
 	CC_PACKAGE=gcc-9
 	;;
 osx-clang)
+	MAKEFLAGS="$MAKEFLAGS PYTHON_PATH=$(which python2)"
 	CC=clang
 	;;
 linux-clang)
