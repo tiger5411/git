d9e0f938e72f3c6b36ca8c8b33eb0e436e5db734
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-02-28 14:28:50 +0100

    revisions API users: add release_revisions() to wt-status.c edge case
    Add a release_revisions() to a use of the "struct rev_info"
    has_unstaged_changes() which didn't require the addition of a "goto"
    pattern, but needed us to refactor another "return fn(...)" pattern.
    
    This change is split from preceding changes to make similar
    conversions in those commits easier to review, as they are all
    repetitions of the same pattern.
    
    Signed-off-by: Ævar Arnfjörð Bjarmason <avarab@gmail.com>

--- wt-status.c
+++ wt-status.c
@@ -2537,7 +2537,9 @@ int has_unstaged_changes(struct repository *r, int ignore_submodules)
 	rev_info.diffopt.flags.quick = 1;
 	diff_setup_done(&rev_info.diffopt);
 	result = run_diff_files(&rev_info, 0);
-	return diff_result_code(&rev_info.diffopt, result);
+	result = diff_result_code(&rev_info.diffopt, result);
+	release_revisions(&rev_info);
+	return result;
 }
 
 /**
