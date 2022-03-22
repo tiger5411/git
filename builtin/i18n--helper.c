a7eff2affbd9b56fc84cac05011ed2c28032bdb7
Ævar Arnfjörð Bjarmason <avarab@gmail.com>
2022-03-22 21:24:59 +0100

    Fucking submodule bull shit

--- builtin/i18n--helper.c
+++ builtin/i18n--helper.c
@@ -3,7 +3,7 @@
 #include "parse-options.h"
 #include "utf8.h"
 
-static char const * const usagestr[] = {
+static char const *const usagestr[] = {
 	N_("git i18n--helper <command-mode> [<arguments>]"),
 	NULL
 };
@@ -12,29 +12,21 @@ static enum cmdmode {
 	I18N_CD_TO_TOPLEVEL = 1,
 } cmdmode;
 
-struct cmdmode2msg {
-	enum cmdmode mode;
-	const char *const msgid;
-	const char *arg1;
-	const char *arg2;
-};
-
-static struct cmdmode2msg cmd2msg[] = {
-	{
-		I18N_CD_TO_TOPLEVEL,
-		N_("Cannot chdir to %s the toplevel of the working tree"),
-		"$cdup",
-	},
-	{ 0 },
-};
-
-static void want_argn(const int want, const int got, const char *const msgid,
-		      const char * const *ustr, const struct option *options)
+static void emit_msg(const int want, const int argc, const char **argv,
+		     const char *const *ustr, const struct option *options,
+		     const char *const msgid)
 {
-	if (want == got)
-		return;
-	usage_msg_optf(_("need %d arguments for '%s' message, got %d"),
-		       ustr, options, want, msgid, got);
+	if (want != argc)
+		usage_msg_optf(_("need %d arguments for '%s' message, got %d"),
+			       ustr, options, want, msgid, argc);
+	switch (argc) {
+	case 1:
+		printf(_(msgid), argv[0]);
+		break;
+	default:
+		BUG("expected %d, not %d arguments for '%s' message", want, argc, msgid);
+	}
+	putchar('\n');
 }
 
 int cmd_i18n__helper(int argc, const char **argv, const char *prefix)
@@ -44,7 +36,6 @@ int cmd_i18n__helper(int argc, const char **argv, const char *prefix)
 			 N_("message for git-sh-setup"), I18N_CD_TO_TOPLEVEL),
 		OPT_END(),
 	};
-	struct cmdmode2msg *c2m = cmd2msg;
 
 	argc = parse_options(argc, argv, prefix, options, usagestr,
 			     0);
@@ -52,23 +43,10 @@ int cmd_i18n__helper(int argc, const char **argv, const char *prefix)
 		usage_msg_opt(_("need a command-mode argument"),
 			      usagestr, options);
 
-	for (c2m = cmd2msg; c2m->mode; c2m++) {
-		if (!c2m->mode)
-			break;
-		if (c2m->mode != cmdmode)
-			continue;
-
-		if (!c2m->arg1) {
-			puts(_(c2m->msgid));
-		} else if (!c2m->arg2) {
-			want_argn(1, argc, c2m->msgid, usagestr, options);
-			printf(_(c2m->msgid), argv[1]);
-			putchar('\n');
-
-void strbuf_utf8_replace(struct strbuf *sb_src, int pos, int width,
-			 const char *subst)
-
-		}
+	switch (cmdmode) {
+	case I18N_CD_TO_TOPLEVEL:
+		emit_msg(1, argc, argv, usagestr, options,
+			 N_("Cannot chdir to %s the toplevel of the working tree"));
 		return 0;
 	}
 	return 0;
