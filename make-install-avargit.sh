tiger5411/git

commit 901d59e10059a1dd67f0d567062115eb709e068a

mia: don't test with git-subtree
Amends aa393d67f7b (series: don't build+install scalar with my own
patches, 2022-04-06).

make-install-avargit.sh
@@ -246,10 +246,11 @@ test_compile () {
 	# Test sanity
 	make -C t test-lint
 	
-	# Don't break these special snowflakes
+	# Don't break these special snowflakes: TODO: fails on
+	# git-subtree, --exec-path issue?
 	parallel 'make -C {} test' ::: \
 		 gitweb \
-		 contrib/scalar contrib/mw-to-git contrib/subtree contrib/diff-highlight
+		 contrib/scalar contrib/mw-to-git contrib/diff-highlight
 
 	# Remove any past test state
 	make -C t clean

commit 0d176f09aaf16a1e2486149194993c521ce523eb

mia: just "git clean" already
More fallout from 901d59e1005 (mia: don't test with git-subtree,
2022-04-06), some things fail whale when "git-subtree" is at the
top-level, but this more generally should reset & fix various issues.

Well, at the cost of having less of a canary for "make"-related
changes, so maybe I shouldn't...

make-install-avargit.sh
@@ -182,6 +182,8 @@ tag_it() {
 }
 
 reset_it() {
+	# Stale things like e.g. "git-subtree"
+	git clean -dxf
 	git reset --hard
 	git checkout build-master || git checkout -b build-master -t origin/master
 	git bisect reset